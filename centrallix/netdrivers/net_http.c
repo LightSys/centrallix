834d51a1c5ef551c4cc627c3d3894928d0428f6a
gbeeley <gbeeley>
2002-05-03 03:52:43 +0000

    Added preliminary session watchdog support to the net_http module. The watchdog timer parameters are controllable via the config file.

--- centrallix/netdrivers/net_http.c
+++ centrallix/netdrivers/net_http.c
@@ -52,10 +52,14 @@
 
 /**CVSDATA***************************************************************
 
-    $Id: net_http.c,v 1.16 2002/05/01 02:20:31 gbeeley Exp $
+    $Id: net_http.c,v 1.17 2002/05/03 03:52:43 gbeeley Exp $
     $Source: /srv/bld/centrallix-repo/centrallix/netdrivers/net_http.c,v $
 
     $Log: net_http.c,v $
+    Revision 1.17  2002/05/03 03:52:43  gbeeley
+    Added preliminary session watchdog support to the net_http module.
+    The watchdog timer parameters are controllable via the config file.
+
     Revision 1.16  2002/05/01 02:20:31  gbeeley
     Modification in net_http: ls__req=close now allows multiple object
     ids to be strung together in the ls__oid parameter.
@@ -179,8 +183,28 @@ typedef struct
     XArray	ErrorList;	/* xarray of xstring */
     XArray	Triggers;	/* xarray of pNhtConnTrigger */
     HandleContext Hctx;
+    handle_t	WatchdogTimer;
+    handle_t	InactivityTimer;
+    int		LinkCnt;
     }
     NhtSessionData, *pNhtSessionData;
+
+
+/*** Timer structure.  The rule on the deallocation of these is that if
+ *** you successfully remove it from the Timers list while holding the
+ *** TimerDataMutex, then the thing is yours to deallocate and no one
+ *** else should be messing with it.
+ ***/
+typedef struct
+    {
+    unsigned long	Time;		/* # msec this goes for */
+    int			(*ExpireFn)();
+    void*		ExpireArg;
+    unsigned long	ExpireTick;	/* internal expiration time mark */
+    int			SeqID;
+    handle_t		Handle;
+    }
+    NhtTimer, *pNhtTimer;
 
 
 /*** GLOBALS ***/
@@ -191,11 +215,333 @@ struct
     pFile	StdOut;
     char	ServerString[80];
     char	Realm[80];
+    pSemaphore	TimerUpdateSem;
+    pSemaphore	TimerDataMutex;
+    XArray	Timers;
+    HandleContext TimerHctx;
+    int		WatchdogTime;
+    int		InactivityTime;
     }
     NHT;
 
 int nht_internal_UnConvertChar(int ch, char** bufptr, int maxlen);
 extern int htrRender(pFile, pObject);
+int nht_internal_RemoveWatchdog(handle_t th);
+
+
+/*** nht_internal_LinkSess() - link to a session, thus increasing its link
+ *** count.
+ ***/
+int
+nht_internal_LinkSess(pNhtSessionData sess)
+    {
+    sess->LinkCnt++;
+    return 0;
+    }
+
+
+/*** nht_internal_UnlinkSess_r() - does the work of freeing an individual 
+ *** handle within the session.
+ ***/
+int
+nht_internal_UnlinkSess_r(void* v)
+    {
+    if (ISMAGIC(v, MGK_OBJSESSION)) 
+	{
+	objCloseSession((pObjSession)v);
+	}
+    return 0;
+    }
+
+
+/*** nht_internal_UnlinkSess() - free a session when its link count reaches 0.
+ ***/
+int
+nht_internal_UnlinkSess(pNhtSessionData sess)
+    {
+    pXString errmsg;
+    int i;
+    pNhtConnTrigger trg;
+
+	/** Bump the link cnt down **/
+	sess->LinkCnt--;
+
+	/** Time to close the session down? **/
+	if (sess->LinkCnt <= 0)
+	    {
+	    printf("NHT: releasing session for username [%s], cookie [%s]\n", sess->Username, sess->Cookie);
+
+	    /** Remove the session from the global session list. **/
+	    xhRemove(&(NHT.CookieSessions), sess->Cookie);
+	    xaRemoveItem(&(NHT.Sessions), xaFindItem(&(NHT.Sessions), (void*)sess));
+
+	    /** First, close all open handles. **/
+	    xhnClearHandles(&(sess->Hctx), nht_internal_UnlinkSess_r);
+
+	    /** Close the master session. **/
+	    objCloseSession(sess->ObjSess);
+
+	    /** Destroy the errors semaphore **/
+	    syDestroySem(sess->Errors, SEM_U_HARDCLOSE);
+
+	    /** Clear the errors list **/
+	    while(sess->ErrorList.nItems)
+		{
+		errmsg = (pXString)(sess->ErrorList.Items[0]);
+		xaRemoveItem(&sess->ErrorList, 0);
+		xsDeInit(errmsg);
+		nmFree(errmsg,sizeof(XString));
+		}
+
+	    /** Clear the triggers list **/
+	    while(sess->Triggers.nItems)
+		{
+		trg = (pNhtConnTrigger)(sess->Triggers.Items[0]);
+		trg->LinkCnt--;
+		if (trg->LinkCnt <= 0)
+		    {
+		    syDestroySem(trg->TriggerSem,SEM_U_HARDCLOSE);
+		    xaRemoveItem(&(sess->Triggers), i);
+		    nmFree(trg, sizeof(NhtConnTrigger));
+		    }
+		}
+
+	    /** Dealloc the xarrays and such **/
+	    xaDeInit(&(sess->Triggers));
+	    xaDeInit(&(sess->ErrorList));
+	    xhnDeInitContext(&(sess->Hctx));
+	    nht_internal_RemoveWatchdog(sess->WatchdogTimer);
+	    nht_internal_RemoveWatchdog(sess->InactivityTimer);
+	    nmFree(sess, sizeof(NhtSessionData));
+	    }
+
+    return 0;
+    }
+
+
+/*** nht_internal_Watchdog() - manages the session watchdog timers.  These
+ *** timers cause sessions to automatically time out if a certain operation
+ *** isn't performed every so often.
+ ***
+ *** fyi - if the tick comparison arithmetic looks funny, it's because we
+ *** need to use modulus arithmetic to prevent failure of this routine if
+ *** (gasp!) Centrallix has been running for several hundred days (at
+ *** CLK_TCK==100, the 32bit tick counter wraps at just under 500 days).
+ ***/
+void
+nht_internal_Watchdog(void* v)
+    {
+    unsigned long cur_tick, next_tick;
+    int i;
+    pNhtTimer t;
+    EventReq timer_event, semaphore_event;
+    pEventReq ev[2] = {&timer_event, &semaphore_event};
+    pNhtTimer expired_timer;
+
+	/** Set the thread's name **/
+	thSetName(NULL,"NHT Watchdog Manager Task");
+
+	/** Enter our loop of compiling timer data and then waiting on
+	 ** either an expiration or on the update semaphore (which tells us
+	 ** that the timer data changed)
+	 **/
+	while(1)
+	    {
+	    /** Acquire the timer data mutex **/
+	    syGetSem(NHT.TimerDataMutex, 1, 0);
+
+	    /** Scan the timer list for next expiration. **/
+	    cur_tick = mtLastTick();
+	    next_tick = cur_tick + 0x7FFFF;
+	    for(i=0;i<NHT.Timers.nItems;i++)
+		{
+		t = (pNhtTimer)(NHT.Timers.Items[i]);
+		if (t->ExpireTick - next_tick > (unsigned long)0x80000000)
+		    {
+		    next_tick = t->ExpireTick;
+		    }
+		}
+	    syPostSem(NHT.TimerDataMutex, 1, 0);
+
+	    /** Has the next tick already expired?  If not, wait for it **/
+	    if (cur_tick - next_tick > (unsigned long)0x80000000)
+		{
+		/** Setup our event list for the multiwait operation **/
+		ev[0]->Object = NULL;
+		ev[0]->ObjType = OBJ_T_MTASK;
+		ev[0]->EventType = EV_T_MT_TIMER;
+		ev[0]->ReqLen = (next_tick - cur_tick)*(1000/CLK_TCK);
+		ev[1]->Object = NHT.TimerUpdateSem;
+		ev[1]->ObjType = OBJ_T_SEM;
+		ev[1]->EventType = EV_T_SEM_GET;
+		ev[1]->ReqLen = 1;
+		thMultiWait(2, ev);
+
+		/** If the semaphore completed, reset the process... **/
+		if (semaphore_event.Status == EV_S_COMPLETE) 
+		    {
+		    /** Clean out any posted updates... **/
+		    while(syGetSem(NHT.TimerUpdateSem, 1, SEM_U_NOBLOCK) >= 0) ;
+
+		    /** Restart the loop and wait for more timers **/
+		    continue;
+		    }
+		}
+
+	    /** Ok, one or more events need to be fired off.
+	     ** We search this over again each time we look for another
+	     ** timer, since the expire routine may have messed with the
+	     ** timer table.
+	     **/
+	    cur_tick = mtLastTick();
+	    do  {
+		syGetSem(NHT.TimerDataMutex, 1, 0);
+		expired_timer = NULL;
+
+		/** Look for a single expired timer **/
+		for(i=0;i<NHT.Timers.nItems;i++)
+		    {
+		    t = (pNhtTimer)(NHT.Timers.Items[i]);
+		    if (t->ExpireTick - cur_tick > (unsigned long)0x80000000)
+			{
+			xaRemoveItem(&(NHT.Timers), i);
+			expired_timer = t;
+			break;
+			}
+		    }
+		syPostSem(NHT.TimerDataMutex, 1, 0);
+
+		/** Here is how the watchdog barks... **/
+		if (expired_timer)
+		    {
+		    expired_timer->ExpireFn(expired_timer->ExpireArg, expired_timer->Handle);
+		    xhnFreeHandle(&(NHT.TimerHctx), expired_timer->Handle);
+		    nmFree(t, sizeof(NhtTimer));
+		    }
+		}
+		while(expired_timer); /* end do loop */
+	    }
+
+
+    thExit();
+    }
+
+
+/*** nht_internal_AddWatchdog() - adds a timer to the watchdog timer list.
+ ***/
+handle_t
+nht_internal_AddWatchdog(int timer_msec, int (*expire_fn)(), void* expire_arg)
+    {
+    pNhtTimer t;
+
+	/** First, allocate ourselves a timer. **/
+	t = (pNhtTimer)nmMalloc(sizeof(NhtTimer));
+	if (!t) return XHN_INVALID_HANDLE;
+	t->ExpireFn = expire_fn;
+	t->ExpireArg = expire_arg;
+	t->Time = timer_msec;
+	t->ExpireTick = mtLastTick() + (timer_msec*CLK_TCK/1000);
+        t->Handle = xhnAllocHandle(&(NHT.TimerHctx), t);
+
+	/** Add it to the list... **/
+	syGetSem(NHT.TimerDataMutex, 1, 0);
+	xaAddItem(&(NHT.Timers), (void*)t);
+	syPostSem(NHT.TimerDataMutex, 1, 0);
+
+	/** ... and post to the semaphore so the watchdog thread sees it **/
+	syPostSem(NHT.TimerUpdateSem, 1, 0);
+
+    return t->Handle;
+    }
+
+
+/*** nht_internal_RemoveWatchdog() - removes a timer from the timer list.
+ *** Returns 0 if it successfully removed it, or -1 if the timer did not
+ *** exist.  In either case, the caller MUST consider the pointer 't'
+ *** as invalid once the call returns.
+ ***/
+int
+nht_internal_RemoveWatchdog(handle_t th)
+    {
+    int idx;
+    pNhtTimer t = xhnHandlePtr(&(NHT.TimerHctx), th);
+
+	if (!t) return -1;
+
+	/** Remove the thing from the list, if it exists. **/
+	syGetSem(NHT.TimerDataMutex, 1, 0);
+	idx = xaFindItem(&(NHT.Timers), (void*)t);
+	if (idx >= 0) xaRemoveItem(&(NHT.Timers), idx);
+	syPostSem(NHT.TimerDataMutex, 1, 0);
+	if (idx < 0) return -1;
+
+	/** Free the timer. **/
+	xhnFreeHandle(&(NHT.TimerHctx), th);
+	nmFree(t, sizeof(NhtTimer));
+
+	/** ... and let the watchdog thread know about it **/
+	syPostSem(NHT.TimerUpdateSem, 1, 0);
+
+    return 0;
+    }
+
+
+/*** nht_internal_ResetWatchdog() - resets the countdown timer for a 
+ *** given watchdog to its original value.  Returns 0 if successful,
+ *** or -1 if the thing is no longer valid.
+ ***/
+int
+nht_internal_ResetWatchdog(handle_t th)
+    {
+    int idx;
+    pNhtTimer t = xhnHandlePtr(&(NHT.TimerHctx), th);
+
+	if (!t) return -1;
+
+	/** Find the timer on the list, if it exists. **/
+	syGetSem(NHT.TimerDataMutex, 1, 0);
+	idx = xaFindItem(&(NHT.Timers), (void*)t);
+	if (idx >= 0) t->ExpireTick = mtLastTick() + (t->Time*CLK_TCK/1000);
+	syPostSem(NHT.TimerDataMutex, 1, 0);
+
+	/** ... and let the watchdog thread know about it **/
+	syPostSem(NHT.TimerUpdateSem, 1, 0);
+
+    return (idx >= 0)?0:-1;
+    }
+
+
+/*** nht_internal_WTimeout() - this routine is called when the session 
+ *** watchdog timer expires for a session.  The response is to remove the
+ *** session.
+ ***/
+int
+nht_internal_WTimeout(void* sess_v)
+    {
+    pNhtSessionData sess = (pNhtSessionData)sess_v;
+
+	/** Unlink from the session. **/
+	nht_internal_UnlinkSess(sess);
+
+    return 0;
+    }
+
+
+/*** nht_internal_ITimeout() - this routine is called when the session 
+ *** inactivity timer expires for a session.  The response is to remove the
+ *** session.
+ ***/
+int
+nht_internal_ITimeout(void* sess_v)
+    {
+    pNhtSessionData sess = (pNhtSessionData)sess_v;
+
+	/** Right now, does the same as WTimeout, so call that. **/
+	nht_internal_WTimeout(sess);
+
+    return 0;
+    }
+
 
 /*** nht_internal_ConstructPathname - constructs the proper OSML pathname
  *** for the open-object operation, given the apparent pathname and url
@@ -304,7 +650,12 @@ nht_internal_WaitTrigger(pNhtSessionData sess, int t_id)
 	    if (trg->TriggerID == t_id)
 	        {
 		trg->LinkCnt++;
-		syGetSem(trg->TriggerSem, 1, 0);
+		if (syGetSem(trg->TriggerSem, 1, 0) < 0)
+		    {
+		    xaRemoveItem(&(sess->Triggers), i);
+		    nmFree(trg, sizeof(NhtConnTrigger));
+		    break;
+		    }
 		trg->LinkCnt--;
 		if (trg->LinkCnt == 0)
 		    {
@@ -331,7 +682,15 @@ nht_internal_ErrorHandler(pNhtSessionData nsess, pFile net_conn)
     char sbuf[256];
 
     	/** Wait on the errors semaphore **/
-	syGetSem(nsess->Errors, 1, 0);
+	if (syGetSem(nsess->Errors, 1, 0) < 0)
+	    {
+	    snprintf(sbuf,256,"HTTP/1.0 200 OK\r\n"
+			 "Server: %s\r\n"
+			 "\r\n"
+			 "<A HREF=/ TARGET=ERR></A>\r\n",NHT.ServerString);
+	    fdWrite(net_conn,sbuf,strlen(sbuf),0,0);
+	    return -1;
+	    }
 
 	/** Grab one error **/
 	errmsg = (pXString)(nsess->ErrorList.Items[0]);
@@ -1168,7 +1527,7 @@ nht_internal_GET(pNhtSessionData nsess, pFile conn, pStruct url_inf)
 
     	/*printf("GET called, stack ptr = %8.8X\n",&cnt);*/
         /** If we're opening the "errorstream", pass of processing to err handler **/
-	if (!strncmp(url_inf->StrVal,"/errorstream",12))
+	if (!strncmp(url_inf->StrVal,"/INTERNAL/errorstream",21))
 	    {
 	    return nht_internal_ErrorHandler(nsess, conn);
 	    }
@@ -1185,6 +1544,7 @@ nht_internal_GET(pNhtSessionData nsess, pFile conn, pStruct url_inf)
 	    fdWrite(conn,sbuf,strlen(sbuf),0,0);
 	    mssPrintError(conn);
 	    netCloseTCP(conn,1000,0);
+	    nht_internal_UnlinkSess(nsess);
 	    thExit();
 	    }
 
@@ -1403,6 +1763,7 @@ nht_internal_PUT(pNhtSessionData nsess, pFile conn, pStruct url_inf, int size, c
 	    fdWrite(conn,sbuf,strlen(sbuf),0,0);
 	    mssPrintError(conn);
 	    netCloseTCP(conn,1000,0);
+	    nht_internal_UnlinkSess(nsess);
 	    thExit();
 	    }
 
@@ -1529,6 +1890,7 @@ nht_internal_COPY(pNhtSessionData nsess, pFile conn, pStruct url_inf, char* dest
 	    fdWrite(conn,sbuf,strlen(sbuf),0,0);
 	    mssPrintError(conn);
 	    netCloseTCP(conn,1000,0);
+	    nht_internal_UnlinkSess(nsess);
 	    thExit();
 	    }
 
@@ -1556,6 +1918,7 @@ nht_internal_COPY(pNhtSessionData nsess, pFile conn, pStruct url_inf, char* dest
 			 "<H1>404 Target Not Found</H1>\r\n",NHT.ServerString);
 	    fdWrite(conn,sbuf,strlen(sbuf),0,0);
 	    netCloseTCP(conn,1000,0);
+	    nht_internal_UnlinkSess(nsess);
 	    thExit();
 	    }
 
@@ -1647,6 +2010,7 @@ nht_internal_ConnHandler(void* conn_v)
     int size=-1;
     int did_alloc = 1;
     int tid = -1;
+    handle_t w_timer, i_timer;
 
     	/*printf("ConnHandler called, stack ptr = %8.8X\n",&s);*/
 
@@ -1792,6 +2156,55 @@ nht_internal_ConnHandler(void* conn_v)
 		    }
 		thSetParam(NULL,"mss",nsess->Session);
 		thSetUserID(NULL,((pMtSession)(nsess->Session))->UserID);
+		w_timer = nsess->WatchdogTimer;
+		i_timer = nsess->InactivityTimer;
+		}
+	    }
+
+	/** Watchdog ping? **/
+	if (!strcmp(urlptr,"/INTERNAL/ping"))
+	    {
+	    if (nsess)
+		{
+		/** Reset only the watchdog timer on a ping. **/
+		if (nht_internal_ResetWatchdog(w_timer))
+		    {
+		    snprintf(sbuf,160,"HTTP/1.0 200 OK\r\n"
+				 "Server: %s\r\n"
+				 "\r\n"
+				 "<A HREF=/ TARGET=ERR></A>\r\n",NHT.ServerString);
+		    fdWrite(conn,sbuf,strlen(sbuf),0,0);
+		    netCloseTCP(conn,1000,0);
+		    thExit();
+		    }
+		}
+	    else
+		{
+		/** No session and this is a watchdog ping?  If so, we don't
+		 ** want to automatically re-login the user since that defeats the purpose
+		 ** of session timeouts.
+		 **/
+		snprintf(sbuf,160,"HTTP/1.0 200 OK\r\n"
+			     "Server: %s\r\n"
+			     "\r\n"
+			     "<A HREF=/ TARGET=ERR></A>\r\n",NHT.ServerString);
+		fdWrite(conn,sbuf,strlen(sbuf),0,0);
+		netCloseTCP(conn,1000,0);
+		thExit();
+		}
+	    }
+	else if (nsess)
+	    {
+	    /** Reset the idle and watchdog timers on a normal request **/
+	    if (nht_internal_ResetWatchdog(i_timer) < 0 || nht_internal_ResetWatchdog(w_timer) < 0)
+		{
+		snprintf(sbuf,160,"HTTP/1.0 200 OK\r\n"
+			     "Server: %s\r\n"
+			     "\r\n"
+			     "<A HREF=/ TARGET=ERR></A>\r\n",NHT.ServerString);
+		fdWrite(conn,sbuf,strlen(sbuf),0,0);
+		netCloseTCP(conn,1000,0);
+		thExit();
 		}
 	    }
 
@@ -1809,13 +2222,16 @@ nht_internal_ConnHandler(void* conn_v)
 	        netCloseTCP(conn,1000,0);
 	        thExit();
 		}
-	    nsess = (pNhtSessionData)nmSysMalloc(sizeof(NhtSessionData));
+	    nsess = (pNhtSessionData)nmMalloc(sizeof(NhtSessionData));
 	    strcpy(nsess->Username, mssUserName());
 	    strcpy(nsess->Password, mssPassword());
 	    nsess->Session = thGetParam(NULL,"mss");
 	    nsess->IsNewCookie = 1;
 	    nsess->ObjSess = objOpenSession("/");
 	    nsess->Errors = syCreateSem(0,0);
+	    nsess->WatchdogTimer = nht_internal_AddWatchdog(NHT.WatchdogTime*1000, nht_internal_WTimeout, (void*)nsess);
+	    nsess->InactivityTimer = nht_internal_AddWatchdog(NHT.InactivityTime*1000, nht_internal_ITimeout, (void*)nsess);
+	    nsess->LinkCnt = 1;
 	    xaInit(&nsess->Triggers,16);
 	    xaInit(&nsess->ErrorList,16);
 	    nht_internal_CreateCookie(nsess->Cookie);
@@ -1823,6 +2239,8 @@ nht_internal_ConnHandler(void* conn_v)
 	    xhAdd(&(NHT.CookieSessions), nsess->Cookie, (void*)nsess);
 	    xaAddItem(&(NHT.Sessions), (void*)nsess);
 	    }
+
+	nht_internal_LinkSess(nsess);
 
 	/** Parse out the requested url **/
 	url_inf = htsParseURL(urlptr);
@@ -1921,6 +2339,8 @@ nht_internal_ConnHandler(void* conn_v)
 	/** End a trigger? **/
 	if (tid != -1) nht_internal_EndTrigger(nsess,tid);
 
+	nht_internal_UnlinkSess(nsess);
+
     thExit();
     }
 
@@ -1984,6 +2404,10 @@ nht_internal_Handler(void* v)
 		{
 		snprintf(NHT.Realm, 80, "Centrallix");
 		}
+
+	    /** Get the timer settings **/
+	    stAttrValue(stLookup(my_config, "session_watchdog_timer"), &(NHT.WatchdogTime), NULL, 0);
+	    stAttrValue(stLookup(my_config, "session_inactivity_timer"), &(NHT.InactivityTime), NULL, 0);
 	    }
 
     	/** Open the server listener socket. **/
@@ -2032,6 +2456,15 @@ nhtInitialize()
 	xhInit(&(NHT.CookieSessions),255,0);
 	xaInit(&(NHT.Sessions),256);
 	NHT.StdOut = fdOpenFD(1,O_RDWR);
+	NHT.TimerUpdateSem = syCreateSem(0, 0);
+	NHT.TimerDataMutex = syCreateSem(1, 0);
+	xhnInitContext(&(NHT.TimerHctx));
+	xaInit(&(NHT.Timers),512);
+	NHT.WatchdogTime = 180;
+	NHT.InactivityTime = 1800;
+
+	/** Start the watchdog timer thread **/
+	thCreate(nht_internal_Watchdog, 0, NULL);
 
 	/** Start the network listener. **/
 	thCreate(nht_internal_Handler, 0, NULL);
