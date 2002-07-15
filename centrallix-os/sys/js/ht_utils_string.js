e993f925121d84e45dc51ce7595f37520c145f7b
lkehresman <lkehresman>
2002-07-15 22:17:34 +0000

    Added a standard header on each .js script with license info

--- centrallix-os/sys/js/ht_utils_string.js
+++ centrallix-os/sys/js/ht_utils_string.js
@@ -1,7 +1,13 @@
-/*
-**  ht_utils_string.js
-**  String manipulation functions
-*/
+// Copyright (C) 1998-2001 LightSys Technology Services, Inc.
+//
+// You may use these files and this library under the terms of the
+// GNU Lesser General Public License, Version 2.1, contained in the
+// included file "COPYING" or http://www.gnu.org/licenses/lgpl.txt.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
 
 function htutil_strpad(str, pad, len) {
 	str = new String(str);
