d3d1aeae5cefbb1c7bb35fc4e5d79b8430137cd0
pfinley <pfinley>
2002-07-26 16:09:56 +0000

    js file for label function(s).

---
+++ centrallix-os/sys/js/htdrv_label.js
@@ -0,0 +1,18 @@
+// Copyright (C) 1998-2001 LightSys Technology Services, Inc.
+//
+// You may use these files and this library under the terms of the
+// GNU Lesser General Public License, Version 2.1, contained in the
+// included file "COPYING" or http://www.gnu.org/licenses/lgpl.txt.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// DO NOT COPY! TOP SECRET FUNCTION!
+function lbl_init(l)
+    {
+    l.kind = 'lbl';
+    l.document.layer = l;
+    return l;
+    }
