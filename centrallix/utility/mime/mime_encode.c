d75c11de999dd54219b209340c84084de33243b4
lkehresman <lkehresman>
2002-08-22 20:12:15 +0000

    Added shell for MIME encoding and decoding

---
+++ centrallix/utility/mime/mime_encode.c
@@ -0,0 +1,65 @@
+/************************************************************************/
+/* Centrallix Application Server System 				*/
+/* Centrallix Core       						*/
+/* 									*/
+/* Copyright (C) 1999-2001 LightSys Technology Services, Inc.		*/
+/* 									*/
+/* This program is free software; you can redistribute it and/or modify	*/
+/* it under the terms of the GNU General Public License as published by	*/
+/* the Free Software Foundation; either version 2 of the License, or	*/
+/* (at your option) any later version.					*/
+/* 									*/
+/* This program is distributed in the hope that it will be useful,	*/
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of	*/
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	*/
+/* GNU General Public License for more details.				*/
+/* 									*/
+/* You should have received a copy of the GNU General Public License	*/
+/* along with this program; if not, write to the Free Software		*/
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  		*/
+/* 02111-1307  USA							*/
+/*									*/
+/* A copy of the GNU General Public License has been included in this	*/
+/* distribution in the file "COPYING".					*/
+/* 									*/
+/* Module: 	libmime							*/
+/* Author:	Luke Ehresman (LME)					*/
+/* Creation:	August 12, 2002						*/
+/* Description:	Provides MIME parsing facilities used mainly in the	*/
+/*		MIME object system driver (objdrv_mime.c)		*/
+/************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "mtask.h"
+#include "mtsession.h"
+#include "obj.h"
+#include "mime.h"
+
+int
+libmime_EncodeQP()
+    {
+    return 0;
+    }
+
+int
+libmime_DecodeQP()
+    {
+    return 0;
+    }
+
+
+int
+libmime_EncodeBase64()
+    {
+    return 0;
+    }
+
+int
+libmime_DecodeBase64()
+    {
+    return 0;
+    }
