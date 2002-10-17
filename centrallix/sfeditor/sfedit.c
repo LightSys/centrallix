ff4ddcfe7bbd6f7b41af6f0df9b52efa2b1f7482
gbeeley <gbeeley>
2002-10-17 18:42:54 +0000

    Added copyright stub...

--- centrallix/sfeditor/sfedit.c
+++ centrallix/sfeditor/sfedit.c
@@ -8,6 +8,51 @@
 #include "mtlexer.h"
 #include "stparse.h"
 
+/************************************************************************/
+/* Centrallix Application Server System 				*/
+/* Centrallix Core       						*/
+/* 									*/
+/* Copyright (C) 1998-2001 LightSys Technology Services, Inc.		*/
+/* 									*/
+/* This program is free software; you can redistribute it and/or modify	*/
+/* it under the terms of the GNU General Public License as published by	*/
+/* the Free Software Foundation; either version 2 of the License, or	*/
+/* (at your option) any later version.					*/
+/* 									*/
+/* This program is distributed in the hope that it will be useful,	*/
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of	*/
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	*/
+/* GNU General Public License for more details.				*/
+/* 									*/
+/* You should have received a copy of the GNU General Public License	*/
+/* along with this program; if not, write to the Free Software		*/
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  		*/
+/* 02111-1307  USA							*/
+/*									*/
+/* A copy of the GNU General Public License has been included in this	*/
+/* distribution in the file "COPYING".					*/
+/* 									*/
+/* Module:	sfedit.c                                                */
+/* Author:	Greg Beeley (GRB)                                       */
+/* Date:	October 17th, 2002                                      */
+/*									*/
+/* Description:	Structure File editor using the GTK widget set.  Used	*/
+/*		to, for instance, build the centrallix.conf server	*/
+/*		configuration file.					*/
+/************************************************************************/
+
+/**CVSDATA***************************************************************
+
+    $Id: sfedit.c,v 1.2 2002/10/17 18:42:54 gbeeley Exp $
+    $Source: /srv/bld/centrallix-repo/centrallix/sfeditor/sfedit.c,v $
+
+    $Log: sfedit.c,v $
+    Revision 1.2  2002/10/17 18:42:54  gbeeley
+    Added copyright stub...
+
+
+ **END-CVSDATA***********************************************************/
+
 
 /*** icon table.  <grunt> move this to a file or something dynamic!! ***/
 #include "x-unknown.unknown.xpm"
