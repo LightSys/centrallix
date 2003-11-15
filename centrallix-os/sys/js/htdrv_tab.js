6351ccdfb789b07851591459590fb54abf2e15b2
gbeeley <gbeeley>
2003-11-15 20:00:27 +0000

    Adding support for tabs on any side of tab control

--- centrallix-os/sys/js/htdrv_tab.js
+++ centrallix-os/sys/js/htdrv_tab.js
@@ -20,34 +20,56 @@ function tc_makecurrent()
 	    t.zIndex = this.tabctl.zIndex - 1;
 	    t.tabpage.visibility = 'hidden';
 	    t.document.images['tb'].src = '/sys/images/tab_lft3.gif';
-	    t.pageY += 2;
-	    t.pageX += 1;
-	    t.clip.height = 23;
+	    t.pageY += this.tabctl.yo;
+	    t.pageX += this.tabctl.xo;
+	    t.clip[this.tabctl.cl] += this.tabctl.ci;
 	    }
 	}
     this.zIndex = this.tabctl.zIndex + 1;
     this.tabpage.visibility = 'inherit';
     this.document.images['tb'].src = '/sys/images/tab_lft2.gif';
-    this.pageY -= 2;
-    this.pageX -= 1;
-    this.clip.height = 25;
+    this.pageY -= this.tabctl.yo;
+    this.pageX -= this.tabctl.xo;
+    this.clip[this.tabctl.cl] -= this.tabctl.ci;
     }
 
 function tc_addtab(l_tab, l_page, l)
     {
-    if (this.tabs.length > 0)
+    if (l.tloc == 0 || l.tloc == 1) // top or bottom
 	{
-	newx = this.tabs[this.tabs.length-1].pageX + this.tabs[this.tabs.length-1].document.width + 1;
-	if (this.tabs[this.tabs.length-1].tabpage.visibility == 'inherit') newx += 1;
+	if (this.tabs.length > 0)
+	    {
+	    newx = this.tabs[this.tabs.length-1].pageX + this.tabs[this.tabs.length-1].document.width + 1;
+	    if (this.tabs[this.tabs.length-1].tabpage.visibility == 'inherit') newx += l.xo;
+	    }
+	else
+	    newx = this.pageX;
 	}
-    else
-	newx = this.pageX;
-    newy = this.pageY - 24;
+    else if (l.tloc == 2) // left
+	newx = this.pageX - l_tab.clip.width + 1;
+    else if (l.tloc == 3) // right
+	newx = this.pageX + this.clip.width - 1;
+
+    if (l.tloc == 2 || l.tloc == 3) // left or right
+	{
+	if (this.tabs.length > 0)
+	    {
+	    newy = this.tabs[this.tabs.length-1].pageY + 26;
+	    if (this.tabs[this.tabs.length-1].tabpage.visibility == 'inherit') newy += l.yo;
+	    }
+	else
+	    newy = this.pageY;
+	}
+    else if (l.tloc == 1) // bottom
+	newy = this.pageY + this.clip.height - 1;
+    else // top
+	newy = this.pageY - 24;
+	    
     if (l_page.visibility != 'inherit')
 	{
-	newx += 1;
-	newy += 2;
-	l_tab.clip.height = 23;
+	newx += l.xo;
+	newy += l.yo;
+	l_tab.clip[l.cl] -= l.ci;
 	}
     for(i=0;i<l_tab.document.images.length;i++)
 	{
@@ -73,7 +95,7 @@ function tc_addtab(l_tab, l_page, l)
     return l_tab;
     }
 
-function tc_init(l)
+function tc_init(l,tloc)
     {
     l.mainlayer = l;
     l.document.layer = l;
@@ -83,7 +105,35 @@ function tc_init(l)
     l.isShorting = 0;
     l.tabs = new Array();
     l.addTab = tc_addtab;
+    l.tloc = tloc;
     if (tc_tabs == null) tc_tabs = new Array();
     tc_tabs[tc_tabs.length++] = l;
+    switch(l.tloc)
+	{
+	case 0: // top
+	    l.xo = +1;
+	    l.yo = +2;
+	    l.cl = "bottom";
+	    l.ci = -2;
+	    break;
+	case 1: // bottom
+	    l.xo = +1;
+	    l.yo = -2;
+	    l.cl = "top";
+	    l.ci = +2;
+	    break;
+	case 2: // left
+	    l.xo = +2;
+	    l.yo = +1;
+	    l.cl = "right";
+	    l.ci = -2;
+	    break;
+	case 3: // right
+	    l.xo = -2;
+	    l.yo = +1;
+	    l.cl = "left";
+	    l.ci = +2;
+	    break;
+	}
     return l;
     }
