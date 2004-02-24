3fda75a658d3d6db0ee80d392adff09574943b8b
gbeeley <gbeeley>
2004-02-24 19:21:23 +0000

    - initial commit of (incomplete) component-declaration widget

---
+++ centrallix-os/sys/js/htdrv_componentdecl.js
@@ -0,0 +1,95 @@
+// Copyright (C) 1998-2001 LightSys Technology Services, Inc.
+//
+// You may use these files and this library under the terms of the
+// GNU Lesser General Public License, Version 2.1, contained in the
+// included file "COPYING" or http://www.gnu.org/licenses/lgpl.txt.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+function cmpd_init(is_visual)
+    {
+    // component is access point for stuff inside, shell is access point for stuff outside.
+    var component = new Object();
+    var shell = new Object();
+    shell.setContext = cmpd_shell_set_context;
+    shell.handleAction = cmpd_shell_handle_action;
+    shell.context = null;
+    component.addAction = cmpd_add_action;
+    component.addEvent = cmpd_add_event;
+    component.addProp = cmpd_add_prop;
+    component.handleAction = cmpd_handle_action;
+    component.ActionModifyProperty = cmpd_action_modify_property;
+    component.shell = shell;
+    component.is_visual = is_visual;
+    shell.component = component;
+    return component;
+    }
+
+function cmpd_endinit(c)
+    {
+    return;
+    }
+
+function cmpd_add_action(a)
+    {
+    this.shell["Action" + a] = new Function('aparam','this.handleAction("' + a + '",aparam);');
+    return;
+    }
+
+function cmpd_add_event(e)
+    {
+    this["Action" + e] = new Function('aparam','this.handleAction("' + e + '",aparam);');
+    return;
+    }
+
+function cmpd_add_prop(p)
+    {
+    this.shell[p] = null;
+    htr_watch(this.shell, p, cmpd_shell_prop_change);
+    return;
+    }
+
+// This is called by the ModifyProperty action in the internal component;
+// update the shell's exposed client prop.  Don't re-trigger an internal
+// event on this unless requested.
+function cmpd_action_modify_property(aparam)
+    {
+    if (aparam.PropertyName && aparam.NewValue) 
+	{
+	if (aparam.TriggerEvent) htr_unwatch(this.shell, aparam.PropertyName, cmpd_shell_prop_change);
+	this.shell[aparam.PropertyName] = aparam.NewValue;
+	if (aparam.TriggerEvent) htr_watch(this.shell, aparam.PropertyName, cmpd_shell_prop_change);
+	}
+    return;
+    }
+
+// this happens when a client prop is changed; cause an internal event on the component
+function cmpd_shell_prop_change(p,o,n)
+    {
+    cn_activate(this.component, 'MODIFY' + p, {OldValue:o, NewValue:n});
+    return n;
+    }
+
+// when an action is called externally, trigger an internal event on the component
+function cmpd_shell_handle_action(a,aparam)
+    {
+    cn_activate(this.component, a, aparam);
+    return;
+    }
+
+// when an action is called internally, trigger an external event on the shell
+function cmpd_handle_action(a,aparam)
+    {
+    cn_activate(this.shell, a, aparam);
+    return;
+    }
+
+// set the context of the component
+function cmpd_shell_set_context(l)
+    {
+    this.context = l;
+    return;
+    }
