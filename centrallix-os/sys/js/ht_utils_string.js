e042c6067d68757fdf7f51ec3e7e5c35f4aefcdc
jasonyip <jasonyip>
2004-03-03 20:23:04 +0000

    Just changed s[i] to s.charAt(i) and it works in all browsers.

--- centrallix-os/sys/js/ht_utils_string.js
+++ centrallix-os/sys/js/ht_utils_string.js
@@ -1,4 +1,4 @@
-// Copyright (C) 1998-2001 LightSys Technology Services, Inc.
+// Copyright (C) 1998-2004 LightSys Technology Services, Inc.
 //
 // You may use these files and this library under the terms of the
 // GNU Lesser General Public License, Version 2.1, contained in the
@@ -9,15 +9,17 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 
-function htutil_strpad(str, pad, len) {
+function htutil_strpad(str, pad, len) 
+    {
     str = new String(str);
     var tmp = '';
     for (var i=0; i < len-str.length; i++)
 	tmp = pad+tmp;
     return tmp+str;
-}
+    }
 
-function htutil_unpack(str) {
+function htutil_unpack(str) 
+    {
     str = new String(str);
     var ret="";
     for(var i=0;i<str.length;i+=2)
@@ -25,46 +27,52 @@ function htutil_unpack(str) {
 	ret+=unescape("%"+str.substr(i,2));
 	}
     return ret;
-}
-
-function htutil_encode(s) {
-    var rs = '';
-    for(var i=0;i<s.length;i++) {
-	if (s[i] == '<') rs += '&lt;';
-	else if (s[i] == '>') rs += '&gt;';
-	else if (s[i] == '&') rs += '&amp;';
-	else if (s[i] == ' ') rs += '&nbsp;';
-	else rs += s[i];
     }
-    return rs;
-}
 
-function htutil_subst_last(str,subst) {
+function htutil_encode(s) 
+    {
+    var rs = '';
+    for(var i=0;i<s.length;i++) 
+        {
+	if (s.charAt(i) == '<') rs += '&lt;';	
+	else if (s.charAt(i) == '>') rs += '&gt;';
+	else if (s.charAt(i) == '&') rs += '&amp;';
+	else if (s.charAt(i) == ' ') rs += '&nbsp;';
+	else rs += s.charAt(i);
+        }
+    return rs;
+    }
+
+function htutil_subst_last(str,subst) 
+    {
     if(!str || !subst)
 	return str;
     return str.substring(0,str.length-subst.length)+subst;
-}
+    }
 
-function htutil_rtrim(str) {
+function htutil_rtrim(str) 
+    {
     for (var i=str.length-1; i>=0 && (str.charAt(i) == ' ' || str.charAt(i)=='\t' || str.charAt(i)=='\xCA'); i--);
     return str.substring(0, i+1);
-}
+    }
 
 /** return common part of string **/
-function htutil_common(str1,str2) {
+function htutil_common(str1,str2) 
+    {
     var cnt = 0;
     if (!str1 || !str2) return "";
     for (var i=0;i<str1.length;i++) if (str1.charAt(i) != str2.charAt(i)) break; else cnt++;
     return str1.substring(0,cnt);
-}
+    }
 
 /** compare two URL's for same server **/
-function htutil_url_cmp(str1,str2) {
+function htutil_url_cmp(str1,str2) 
+    {
     var common = htutil_common(str1,str2);
     var cmp_re = /^http:\/+[a-zA-Z._0-9:]+\//i;
     if (common.search(cmp_re) >= 0)
 	return true;
     else
 	return false;
-}
+    }
 
