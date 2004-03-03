f02600978d102795d228d3d9731198a10d91bbc1
jasonyip <jasonyip>
2004-03-03 19:48:25 +0000

    This editbox has merged the chnages with the new presentation hints and IE port.

--- centrallix-os/sys/js/htdrv_editbox.js
+++ centrallix-os/sys/js/htdrv_editbox.js
@@ -1,4 +1,4 @@
-// Copyright (C) 1998-2001 LightSys Technology Services, Inc.
+// Copyright (C) 1998-2004 LightSys Technology Services, Inc.
 //
 // You may use these files and this library under the terms of the
 // GNU Lesser General Public License, Version 2.1, contained in the
@@ -22,7 +22,7 @@ function eb_update_cursor(eb,val)
 	}
     if(eb.cursorlayer == ibeam_current)
 	{
-	ibeam_current.moveToAbsolute(eb.ContentLayer.pageX + eb.cursorCol*text_metric.charWidth, eb.ContentLayer.pageY);
+	moveToAbsolute(ibeam_current, getPageX(eb.ContentLayer) + eb.cursorCol*text_metric.charWidth, getPageX(eb.ContentLayer));
 	}
     }
 
@@ -40,14 +40,17 @@ function eb_clearvalue()
 
 function eb_enable()
     {
-    eval('this.document.'+this.bg);
+    if (cx__capabilities.Dom0IE)
+        pg_set_style(this,'bgColor',this.bg);
+    else
+	eval('this.document.'+this.xbg);
     this.enabled='full';
     }
 
 function eb_disable()
     {
     this.document.background='';
-    this.document.bgColor='#e0e0e0';
+    pg_set_style(this.document, 'bgColor','#e0e0e0');
     this.enabled='disabled';
     }
 
@@ -59,10 +62,18 @@ function eb_readonly()
 
 function eb_settext(l,txt)
     {
-    l.HiddenLayer.document.write('<PRE>' + htutil_encode(txt) + '</PRE> ');
-    l.HiddenLayer.document.close();
-    l.HiddenLayer.visibility = 'inherit';
-    l.ContentLayer.visibility = 'hidden';
+    if (cx__capabilities.Dom0NS)
+        {
+    	l.HiddenLayer.document.write('<PRE>' + htutil_encode(txt) + '</PRE> ');    	
+    	l.HiddenLayer.document.close();    	
+    	}
+    else if (cx__capabilities.Dom1HTML)
+        {        
+    	l.HiddenLayer.innerHTML = '<PRE>' + htutil_encode(txt) + '</PRE> ';
+        }
+    pg_set_style(l.HiddenLayer, 'visibility', 'inherit');
+    pg_set_style(l.ContentLayer, 'visibility', 'hidden');
+    
     var tmp = l.ContentLayer;
     l.ContentLayer = l.HiddenLayer;
     l.HiddenLayer = tmp;
@@ -77,7 +88,7 @@ function eb_keyhandler(l,e,k)
     var adj = 0;
     var txt = l.content;
     var newtxt;
-    var charClip = Math.ceil((l.pageX - l.ContentLayer.pageX) / text_metric.charWidth);
+    var charClip = Math.ceil((getPageX(l) - getPageX(l.ContentLayer)) / text_metric.charWidth);
     var relPos = l.cursorCol - charClip;
     if (k == 9)
 	{
@@ -124,12 +135,12 @@ function eb_keyhandler(l,e,k)
 	{
 	return true;
 	}
-    ibeam_current.visibility = 'hidden';
+    pg_set_style(ibeam_current, 'visibility', 'hidden');
     eb_settext(l,newtxt);
-    l.ContentLayer.pageX += adj;
-    l.HiddenLayer.pageX += adj;
-    ibeam_current.moveToAbsolute(l.ContentLayer.pageX + l.cursorCol*text_metric.charWidth, l.ContentLayer.pageY);
-    ibeam_current.visibility = 'inherit';
+    setPageX(l.ContentLayer, getPageX(l.ContentLayer) + adj);
+    setPageX(l.HiddenLayer, getPageX(l.HiddenLayer)+ adj);
+    moveToAbsolute(ibeam_current, getPageX(l.ContentLayer) + l.cursorCol*text_metric.charWidth, getPageY(l.ContentLayer));
+    pg_set_style(ibeam_current,'visibility', 'inherit');
     l.changed=true;
     cn_activate(l,"DataChange");
     return false;
@@ -142,40 +153,54 @@ function eb_select(x,y,l,c,n)
     if (x == null && y == null)
 	l.cursorCol = l.content.length;
     else
-	l.cursorCol = Math.round((x + l.pageX - l.ContentLayer.pageX)/text_metric.charWidth);
+	l.cursorCol = Math.round((x + getPageX(l) - getPageX(l.ContentLayer))/text_metric.charWidth);
     if (l.cursorCol > l.content.length) l.cursorCol = l.content.length;
-    if (eb_current) eb_current.cursorlayer = null;
-    eb_current = l;
-    eb_current.cursorlayer = ibeam_current;
-    ibeam_current.visibility = 'hidden';
-    ibeam_current.moveAbove(eb_current);
-    ibeam_current.moveToAbsolute(eb_current.ContentLayer.pageX + eb_current.cursorCol*text_metric.charWidth, eb_current.ContentLayer.pageY);
-    ibeam_current.zIndex = eb_current.zIndex + 2;
-    ibeam_current.visibility = 'inherit';
+    if (eb_current) eb_current.cursorlayer = null;     
+    eb_current = l;    
+    eb_current.cursorlayer = ibeam_current;    
+    pg_set_style(ibeam_current, 'visibility', 'hidden');
+    moveAbove(ibeam_current,eb_current);
+    moveToAbsolute(ibeam_current, getPageX(eb_current.ContentLayer) + eb_current.cursorCol*text_metric.charWidth, getPageY(eb_current.ContentLayer));    
+    pg_set_style(ibeam_current, 'zIndex', pg_get_style(eb_current,'zIndex') + 2);
+    pg_set_style(ibeam_current, 'visibility', 'inherit');
     cn_activate(l,"GetFocus");
     return 1;
     }
 
 function eb_deselect()
     {
-    ibeam_current.visibility = 'hidden';
+    pg_set_style(ibeam_current, 'visibility', 'hidden');
     cn_activate(eb_current,"LoseFocus");
     if (eb_current)
 	{
 	eb_current.cursorlayer = null;
 	if (eb_current.changed) eb_current.changed=false;
-	eb_current.ContentLayer.pageX = eb_current.pageX+1;
-	eb_current.HiddenLayer.pageX = eb_current.pageX+1;
+	setPageX(eb_current.ContentLayer, getPageX(eb_current)+1);
+	setPageX(eb_current.HiddenLayer, getPageX(eb_current)+1);
 	eb_current = null;
 	}
     return true;
     }
 
+/**
+* l - base layer
+* c1 - content layer 1
+* c2 - content layer 2 - hidden
+* is_readonly - if the editbox is read only
+* main_bg - background color
+**/
 function eb_init(l,c1,c2,fieldname,is_readonly,main_bg)
     {
     if (!main_bg)
 	{
-	l.bg = "bgcolor='#c0c0c0'";
+	if (cx__capabilities.Dom0NS)
+	    {
+	    l.bg = "bgcolor='#c0c0c0'";
+	    }
+	else if (cx__capabilities.Dom0IE)
+	    {
+	    l.bg = "backgroundColor='#c0c0c0'";
+	    }
 	}
     else
 	{
@@ -188,7 +213,7 @@ function eb_init(l,c1,c2,fieldname,is_readonly,main_bg)
     l.HiddenLayer = c2;
     l.fieldname = fieldname;
     ibeam_init();
-    l.charWidth = Math.floor((l.clip.width-2)/text_metric.charWidth);
+    l.charWidth = Math.floor((getClipWidth(l)-2)/text_metric.charWidth);
     c1.mainlayer = l;
     c2.mainlayer = l;
     c1.kind = 'eb';
@@ -217,9 +242,9 @@ function eb_init(l,c1,c2,fieldname,is_readonly,main_bg)
 	l.enabled = 'full';
 	}
     l.isFormStatusWidget = false;
-    pg_addarea(l, -1,-1,l.clip.width+1,l.clip.height+1, 'ebox', 'ebox', is_readonly?0:3);
-    c1.y = ((l.clip.height - text_metric.charHeight)/2);
-    c2.y = ((l.clip.height - text_metric.charHeight)/2);
+    pg_addarea(l, -1,-1,getClipWidth(l)+1,getClipHeight(l)+1, 'ebox', 'ebox', is_readonly?0:3);
+    setRelativeY(c1, (getClipHeight(l) - text_metric.charHeight)/2);
+    setRelativeY(c2, (getClipHeight(l) - text_metric.charHeight)/2);
     if (fm_current) fm_current.Register(l);
     l.form = fm_current;
     l.changed = false;
