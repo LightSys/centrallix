9cefb17bdf21f0d58ee610969017027681d51cae
gbeeley <gbeeley>
2006-04-07 06:00:15 +0000

    - (change) convert widgets to new inter-widget interface mechanism rather   than direct callbacks for Events and Actions - (change) incorporate event scripts into .js file - (performance) minimize image reloads, lessening flicker

--- centrallix-os/sys/js/htdrv_imagebutton.js
+++ centrallix-os/sys/js/htdrv_imagebutton.js
@@ -12,13 +12,13 @@
 function ib_enable()
     {
     this.enabled=true;
-    pg_set(this.img,'src',this.nImage.src);
+    ib_setmode(this, 'n');
     }
 
 function ib_disable()
     {
     this.enabled=false;
-    pg_set(this.img,'src',this.dImage.src);
+    ib_setmode(this, 'd');
     }
 
 function ib_setenable(prop,oldval,newval)
@@ -26,12 +26,107 @@ function ib_setenable(prop,oldval,newval)
     var e = window.event;    
     var source = new Object();    
     (cx__capabilities.Dom0IE)? source = e.srcElement : source = this;
+    if (newval == oldval) return newval;
     
     if (newval)
-	pg_set(source.img,'src',source.nImage.src);
+	ib_setmode(source, 'n');
     else
-	pg_set(source.img,'src',source.dImage.src);
+	ib_setmode(source, 'd');
     return newval;
+    }
+
+function ib_setmode(ly, mode)
+    {
+    var newsrc = ly.cursrc;
+    switch(mode)
+        {
+	case 'n':
+	    newsrc = ly.nImage.src;
+	    break;
+	case 'p':
+	    newsrc = ly.pImage.src;
+	    break;
+	case 'c':
+	    newsrc = ly.cImage.src;
+	    break;
+	case 'd':
+	    newsrc = ly.dImage.src;
+	    break;
+	}
+    if (newsrc == ly.cursrc) return;
+    ly.cursrc = newsrc;
+    pg_set(ly.img, 'src', newsrc);
+    }
+
+function ib_mousedown(e)
+    {
+    var ly = (typeof e.target.layer != "undefined" && e.target.layer != null)?e.target.layer:e.target;
+    if (ly.kind=='ib' && ly.layer.enabled==true)
+        {
+	ib_setmode(ly.layer, 'c');
+        cn_activate(ly.layer, 'MouseDown');
+        ib_cur_img = ly.layer.img;
+        }
+    return EVENT_ALLOW_DEFAULT_ACTION | EVENT_CONTINUE;
+    }
+
+function ib_mouseup(e)
+    {
+    var ly = (typeof e.target.layer != "undefined" && e.target.layer != null)?e.target.layer:e.target;
+    if (ib_cur_img)
+        {
+        if (e.pageX >= getPageX(ib_cur_img.layer) &&
+            e.pageX < getPageX(ib_cur_img.layer) + getClipWidth(ib_cur_img.layer) &&
+            e.pageY >= getPageY(ib_cur_img.layer) &&
+            e.pageY < getPageY(ib_cur_img.layer) + getClipHeight(ib_cur_img.layer))
+            {
+            cn_activate(ly.layer, 'Click');
+            cn_activate(ly.layer, 'MouseUp');
+	    ib_setmode(ib_cur_img.layer, 'p');
+            }
+        else
+            {
+	    ib_setmode(ib_cur_img.layer, 'n');
+            }
+        ib_cur_img = null;
+        }
+    return EVENT_ALLOW_DEFAULT_ACTION | EVENT_CONTINUE;
+    }
+
+function ib_mousemove(e)
+    {
+    var ly = (typeof e.target.layer != "undefined" && e.target.layer != null)?e.target.layer:e.target;
+    if (ly.kind == 'ib' && ly.layer.enabled == true)
+        {
+        ly = ly.layer;
+        if (ly.img && ly.img.src != ly.cImage.src) ib_setmode(ly, 'p'); 
+        cn_activate(ly.layer, 'MouseMove');
+        }
+    return EVENT_ALLOW_DEFAULT_ACTION | EVENT_CONTINUE;
+    }
+
+function ib_mouseover(e)
+    {
+    var ly = (typeof e.target.layer != "undefined" && e.target.layer != null)?e.target.layer:e.target;
+    if (ly.kind == 'ib' && ly.layer.enabled == true)
+        {
+        ly = ly.layer;
+        if (ly.img && (ly.img.src != ly.cImage.src)) ib_setmode(ly, 'p');
+        cn_activate(ly.layer, 'MouseOver');
+        }
+    return EVENT_ALLOW_DEFAULT_ACTION | EVENT_CONTINUE;
+    }
+
+function ib_mouseout(e)
+    {
+    var ly = (typeof e.target.layer != "undefined" && e.target.layer != null)?e.target.layer:e.target;
+    if (ly.kind == 'ib' && ly.layer.enabled == true)
+        {
+        ly = ly.layer;
+        if (ly.img && ly.img.src != ly.cImage.src) ib_setmode(ly, 'n'); 
+        cn_activate(ly.layer, 'MouseMove');
+        }
+    return EVENT_ALLOW_DEFAULT_ACTION | EVENT_CONTINUE;
     }
 
 function ib_init(param)
@@ -53,11 +148,12 @@ function ib_init(param)
 	{
 	alert('browser not supported');
 	}
+    htr_init_layer(l,l, 'ib');
+    l.layer = l;
+    ifc_init_widget(l);
     l.img.layer = l;
     l.img.kind = 'ib';
-    l.kind = 'ib';
-    l.layer = l;
-    l.mainlayer = l;
+    l.cursrc = param.n;
     setClipWidth(l, w);
 
     l.buttonName = param.name;
@@ -73,8 +169,6 @@ function ib_init(param)
     if (h == -1) l.dImage = new Image();
     else l.dImage = new Image(w,h);
     pg_set(l.dImage,'src',param.d);
-    l.ActionEnable = ib_enable;
-    l.ActionDisable = ib_disable;
     l.enabled = null;
     if (cx__capabilities.Dom0IE)
     	{
@@ -84,5 +178,21 @@ function ib_init(param)
     	{
 	l.watch("enabled",ib_setenable);
 	}
+
+    // Actions
+    var ia = l.ifcProbeAdd(ifAction);
+    ia.Add("Enable", ib_enable);
+    ia.Add("Disable", ib_disable);
+
+    // Events
+    var ie = l.ifcProbeAdd(ifEvent);
+    ie.Add("Click");
+    ie.Add("MouseDown");
+    ie.Add("MouseUp");
+    ie.Add("MouseOver");
+    ie.Add("MouseOut");
+    ie.Add("MouseMove");
+    
     l.enabled = param.enable;
     }
+
