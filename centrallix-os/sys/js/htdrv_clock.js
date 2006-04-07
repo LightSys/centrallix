932ca4ebec1e243ccaaa9526b0bde4be1dc28cf3
gbeeley <gbeeley>
2006-04-07 05:52:32 +0000

    - (feature) port to mozilla. - (bugfix) in military time, pad single-digit hrs. - (bugfix) in non-military time, 00 is 12 am rather than 00 am. - (change) convert widgets to new inter-widget interface mechanism rather   than direct callbacks for Events and Actions.

--- centrallix-os/sys/js/htdrv_clock.js
+++ centrallix-os/sys/js/htdrv_clock.js
@@ -16,14 +16,11 @@ function cl_init(param){
 	var f = param.fieldname;
 	var bg = param.background;
 
-        c1.document.layer = c1;
-        c2.document.layer = c2;
-        c1.mainlayer=l;
-        c2.mainlayer=l;
-        l.mainlayer=l;
-        c1.kind='cl';
-        c2.kind='cl';
-        l.kind='cl';
+	//alert(getPageX(l) + "," + getPageY(l));
+	//alert(l.id);
+	htr_init_layer(l,l,'cl');
+	htr_init_layer(c1,l,'cl');
+	htr_init_layer(c2,l,'cl');
         l.contentLayer = c1;
         l.hiddenLayer = c2;
         l.shadowed = param.shadowed;
@@ -36,16 +33,26 @@ function cl_init(param){
         l.showAmPm = param.showAmPm;
         l.milTime = param.milTime;
         if (param.shadowed==1) {
-                l.contentLayer.shadowLayer = new Layer(l.document.width-param.sox,l);
-                l.contentLayer.shadowLayer.moveTo(param.sox,param.soy);
-                l.contentLayer.shadowLayer.zIndex = l.contentLayer.zIndex-1;
-                l.contentLayer.shadowLayer.visibility = 'inherit';
-                l.hiddenLayer.shadowLayer = new Layer(l.document.width-param.sox,l);
-                l.hiddenLayer.shadowLayer.moveTo(param.sox,param.soy);
-                l.hiddenLayer.shadowLayer.zIndex = l.hiddenLayer.zIndex-1;
-                l.hiddenLayer.shadowLayer.visibility = 'hidden';
+		l.contentLayer.shadowLayer = htr_new_layer(getClipWidth(l)-param.sox,l);
+		moveTo(l.contentLayer.shadowLayer, param.sox, param.soy);
+		htr_setzindex(l.contentLayer.shadowLayer, htr_getzindex(l.contentLayer)-1);
+		htr_setvisibility(l.contentLayer.shadowLayer, 'inherit');
+                l.hiddenLayer.shadowLayer = htr_new_layer(getClipWidth(l)-param.sox,l);
+                moveTo(l.hiddenLayer.shadowLayer, param.sox, param.soy);
+                htr_setzindex(l.hiddenLayer.shadowLayer, htr_getzindex(l.hiddenLayer)-1);
+                htr_setvisibility(l.hiddenLayer.shadowLayer, 'hidden');
         }
         cl_update_time(l);
+
+	// Events
+	ifc_init_widget(l);
+	var ie = l.ifcProbeAdd(ifEvent);
+	ie.Add("MouseUp");
+	ie.Add("MouseDown");
+	ie.Add("MouseOver");
+	ie.Add("MouseMove");
+	ie.Add("MouseOut");
+
         return l;
 }
 
@@ -62,16 +69,16 @@ function cl_get_time(l) {
 
 function cl_update_time(l) {
 	var time = cl_get_time(l);
-	l.hiddenLayer.document.write("<table width="+l.document.width+" height="+l.document.height+" border=0><tr><td valign='center' align='center' nowrap><font size='+"+l.fontSize+"' color='"+l.fgColor1+"'><pre>"+((l.bold)?'<b>':'')+time.formated+((l.bold)?'</b>':'')+"</pre></font></td></tr></table>");
-	l.hiddenLayer.document.close();
+	var str = "<table width="+getClipWidth(l)+" height="+getClipHeight(l)+" border=0><tr><td valign='center' align='center' nowrap><font size='+"+l.fontSize+"' color='"+l.fgColor1+"'><pre style='margin:0px; padding:0px;'>"+((l.bold)?'<b>':'')+time.formated+((l.bold)?'</b>':'')+"</pre></font></td></tr></table>";
+	htr_write_content(l.hiddenLayer, str);
 	if (l.shadowed) {
-		l.hiddenLayer.shadowLayer.document.write("<table width="+l.document.width+" height="+l.document.height+" border=0><tr><td valign='center' align='center' nowrap><font size='+"+l.fontSize+"' color='"+l.fgColor2+"'><pre>"+((l.bold)?'<b>':'')+time.formated+((l.bold)?'</b>':'')+"</pre></font></td></tr></table>");
-		l.hiddenLayer.shadowLayer.document.close();
-		l.contentLayer.shadowLayer.visibility = 'hidden';
-		l.hiddenLayer.shadowLayer.visibility = 'inherit';
+		str = "<table width="+getClipWidth(l)+" height="+getClipHeight(l)+" border=0><tr><td valign='center' align='center' nowrap><font size='+"+l.fontSize+"' color='"+l.fgColor2+"'><pre style='margin:0px; padding:0px;'>"+((l.bold)?'<b>':'')+time.formated+((l.bold)?'</b>':'')+"</pre></font></td></tr></table>";
+		htr_write_content(l.hiddenLayer.shadowLayer, str);
+		htr_setvisibility(l.contentLayer.shadowLayer, 'hidden');
+		htr_setvisibility(l.hiddenLayer.shadowLayer, 'inherit');
 	}
-	l.contentLayer.visibility = 'hidden';
-	l.hiddenLayer.visibility = 'inherit';
+	htr_setvisibility(l.contentLayer, 'hidden');
+	htr_setvisibility(l.hiddenLayer, 'inherit');
 	var tmp = l.contentLayer;
 	l.contentLayer = l.hiddenLayer;
 	l.hiddenLayer = tmp;
@@ -93,6 +100,12 @@ function cl_format_time(l,t) {
 		if (t.hrs > 12) t.hrs = t.hrs - 12;
 	}
 	else t.ampm = "am";
+	if (t.hrs == 0 && !l.milTime) 
+	    t.hrs += 12;
+	if (l.miltime) 
+	    t.hrs = htutil_strpad(t.hrs, '0', 2);
+	else
+	    t.hrs = htutil_strpad(t.hrs, ' ', 2);
 	var timef = t.hrs+":"+t.mins+((l.showSecs)?":"+t.secs:"")+((l.showAmPm)?(" "+t.ampm):"");
 	return timef;
 }
