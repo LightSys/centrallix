3d2c28efcae653c898a3c7050a9441be3bd77d24
gbeeley <gbeeley>
2007-03-21 04:36:40 +0000

    - (feature) adding parameter widget to handle app or component parameters   and make the value and hints available on the client.

---
+++ centrallix-os/sys/js/htdrv_parameter.js
@@ -0,0 +1,64 @@
+// Copyright (C) 1998-2007 LightSys Technology Services, Inc.
+//
+// You may use these files and this library under the terms of the
+// GNU Lesser General Public License, Version 2.1, contained in the
+// included file "COPYING" or http://www.gnu.org/licenses/lgpl.txt.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+function pa_actionsetvalue(aparam)
+    {
+    if (aparam.Value) 
+	{
+	this.setvalue(cx_hints_checkmodify(this, this.value, aparam.Value, this.datatype));
+	}
+    }
+
+function pa_getvalue()
+    {
+    return this.value;
+    }
+
+function pa_setvalue(v)
+    {
+    this.value = v;
+    }
+
+function pa_verify()
+    {
+    cx_hints_startnew(this);
+    this.value = cx_hints_checkmodify(this, this.value, this.newvalue, this.datatype);
+    }
+
+function pa_init(l, wparam)
+    {
+    ifc_init_widget(l);
+
+    // Params
+    l.newvalue = wparam.val;
+    l.value = null;
+    if (l.newvalue != null)
+	{
+	l.newvalue = htutil_unpack(l.newvalue);
+	}
+    l.datatype = wparam.type;
+
+    // Callbacks
+    l.getvalue = pa_getvalue;
+    l.setvalue = pa_setvalue;
+
+    l.Verify = pa_verify;
+
+    // Events
+    var ie = l.ifcProbeAdd(ifEvent);
+    ie.Add("DataChange");
+
+    // Actions
+    var ia = l.ifcProbeAdd(ifAction);
+    ia.Add("SetValue", pa_actionsetvalue);
+
+    return l;
+    }
