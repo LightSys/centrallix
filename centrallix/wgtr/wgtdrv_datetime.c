01e4a00d1d53919617001de9c1a2c8cd41a3638b
gbeeley <gbeeley>
2007-06-06 15:18:56 +0000

    - (change) allow datetime widget to grow/shrink horizontally if no flex   is specified

--- centrallix/wgtr/wgtdrv_datetime.c
+++ centrallix/wgtr/wgtdrv_datetime.c
@@ -62,7 +62,7 @@ wgtdtVerify(pWgtrVerifySession s)
 int
 wgtdtNew(pWgtrNode node)
     {   
-	if(node->fl_width < 0) node->fl_width = 0;
+	if(node->fl_width < 0) node->fl_width = 40;
 	if(node->fl_height < 0) node->fl_height = 0;
 	
     return 0;
