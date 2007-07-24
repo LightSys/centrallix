e082909f5ab13d398eac94bd5f7825fcbaa45352
dkasper <dkasper>
2007-07-24 23:03:50 +0000

    - The autolayout widget needed to be modified to allow the objects of a   repeat widget to be placed inside of it (since the repeat widget is a   non-visual widget and previously it was not allowed to put a non-visual   widget inside of an hbox/vbox).

--- centrallix/wgtr/wgtdrv_autolayout.c
+++ centrallix/wgtr/wgtdrv_autolayout.c
@@ -51,14 +51,14 @@ int
 wgtalVerify(pWgtrVerifySession s)
     {
     pWgtrNode al = s->CurrWidget;
-    pWgtrNode child;
-    pWgtrNode sortarray[32];
-    int ord[32];
+    pWgtrNode child,rptchild;
+    pWgtrNode sortarray[64];
+    int ord[64];
     int xo, yo;
     int al_type = -1;
-    int i, j, ins_at;
+    int i, j, ins_at, rpti;
     char* ptr;
-    int count;
+    int count,rptcount=0,rpttotal=0,insertcount=0;
     int n;
     int spacing;
     int cellsize = -1;
@@ -109,16 +109,75 @@ wgtalVerify(pWgtrVerifySession s)
 		}
 
 	    /** Grab the widgets, and sort them according to autolayout_order, if supplied. **/
-	    for(i=0;i<count;i++)
+	    //mssError(1,"WGTRAL","Count=%d",count);
+	    for(i=0;i<count;i++) //+rpttotal
 		{
-		child = (pWgtrNode)(xaGetItem(&(al->Children), i));
-		if (child->Flags & WGTR_F_NONVISUAL)
+		child = (pWgtrNode)(xaGetItem(&(al->Children), i)); //-rpttotal
+		//mssError(1,"WGTRAL", "Found Child %s",child->Name);
+		if (child->Flags & WGTR_F_NONVISUAL && child->Flags & WGTR_F_CONTAINER)
+		    {
+		    //mssError(1, "WGTRAL", "Repeat widget found...");
+		    //count--; //don't count this widget
+		    rptcount = xaCount(&(child->Children));
+		    //mssError(1, "WGTRAL", "rpttotal=%d,count=%d",rpttotal,count);
+		    if (count + rpttotal > sizeof(sortarray)/sizeof(pWgtrNode))
+			{
+			mssError(1, "WGTRAL", "Too many widgets inside %s '%s'", child->Type, child->Name);
+			return -1;
+			}
+		    else
+			{
+			//do the normal routine only on the repeat widget's children instead of on the repeat widget
+			//mssError(1, "WGTRAL", "%d repeat children found", rptcount);
+			for(rpti=0;rpti<rptcount;rpti++)
+			    {
+			    rptchild = (pWgtrNode)(xaGetItem(&(child->Children),rpti));
+			    ins_at=i;
+			    if (rptchild->Flags & WGTR_F_NONVISUAL)
+				{
+				mssError(1, "WGTRAL", "Cannot place nonvisual widget '%s' inside repeat inside %s '%s'",rptchild->Name,al->Type,al->Name);
+				return -1;
+				}
+				ins_at = insertcount;
+				if(wgtrGetPropertyValue(rptchild, "autolayout_order", DATA_T_INTEGER, POD(&n)) == 0)
+				    {
+				    for(j=0;j<i+rpttotal;j++)
+					{
+					if (ord[j] > n)
+					    {
+					    ins_at = j;
+					    break;
+					    }
+					}
+				    }
+				else
+				    {
+				    if (i)
+					n = ord[i-1];
+				    else
+					n = 0;
+				    }
+				if (ins_at < i)
+				    {
+				    memmove(sortarray+ins_at+1, sortarray+ins_at, sizeof(pWgtrNode)*(i-ins_at));
+				    memmove(ord+ins_at+1, ord+ins_at, sizeof(int)*(i-ins_at));
+				    }
+				//mssError(1, "WGTRAL", "i=%d, rpti=%d, ins_at=%d",i,rpti,ins_at);
+				sortarray[ins_at] = rptchild;
+				ord[ins_at] = n;
+				//i++;
+				insertcount++;
+			    }
+			    rpttotal += rptcount;
+			}
+		    }
+		else if (child->Flags & WGTR_F_NONVISUAL)
 		    {
 		    mssError(1, "WGTRAL", "Cannot place nonvisual widget '%s' inside %s '%s'", 
 			    child->Name, al->Type, al->Name);
 		    return -1;
 		    }
-		ins_at = i;
+		ins_at = insertcount;
 		if (wgtrGetPropertyValue(child, "autolayout_order", DATA_T_INTEGER, POD(&n)) == 0)
 		    {
 		    for(j=0;j<i;j++)
@@ -144,11 +203,14 @@ wgtalVerify(pWgtrVerifySession s)
 		    }
 		sortarray[ins_at] = child;
 		ord[ins_at] = n;
+		insertcount++;
+		//mssError(1,"WGTRAL","i=%d,Count=%d,rpttotal=%d,insertcount=%d",i,count,rpttotal,insertcount);
 		}
 
+		//mssError(1,"WGTRAL","i=%d,Count=%d,rpttotal=%d",i,count,rpttotal);
 	    /** Ok, now set the x and y for all the widgets **/
 	    xo = yo = 0;
-	    for(i=0;i<count;i++)
+	    for(i=0;i<count+rpttotal;i++)
 		{
 		child = sortarray[i];
 		if (al_type == 0)	/* hbox */
@@ -183,6 +245,7 @@ wgtalVerify(pWgtrVerifySession s)
 			yo += child->r_height;
 		    yo += spacing;
 		    }
+		//mssError(1, "WGTRAL","x=%d,r_x=%d,pre_x=%d,y=%d",child->x,child->r_x,child->pre_x,child->y);
 		}
 
 	    /** Set container size if needed **/
