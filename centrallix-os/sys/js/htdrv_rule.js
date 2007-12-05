1c06bedaca547f2a02e823a80a3431aa799552bf
gbeeley <gbeeley>
2007-12-05 19:35:00 +0000

    - (feature) declarative rule widget.

---
+++ centrallix-os/sys/js/htdrv_rule.js
@@ -0,0 +1,23 @@
+// Copyright (C) 1998-2007 LightSys Technology Services, Inc.
+//
+// You may use these files and this library under the terms of the
+// GNU Lesser General Public License, Version 2.1, contained in the
+// included file "COPYING" or http://www.gnu.org/licenses/lgpl.txt.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+function rl_init(node, ruletype, param)
+    {
+    node.kind = 'rl';
+    node.ruletype = ruletype;
+
+    for(var paramname in param)
+	{
+	node[paramname] = param[paramname];
+	}
+
+    return node;
+    }
