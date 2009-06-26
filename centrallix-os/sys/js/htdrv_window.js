1979ef419dc4954d502c587b4892ab2c43b97415
gbeeley <gbeeley>
2009-06-26 21:37:13 +0000

    - (feature) new Popup action, which pops up a window at a given location

--- centrallix-os/sys/js/htdrv_window.js
+++ centrallix-os/sys/js/htdrv_window.js
@@ -11,6 +11,8 @@
 
 //$(".wn")
 
+var wn_popped = {};
+
 function wn_deinit()
     {
     // If we moved it to top level, move it back so it can get cleaned up.
@@ -111,6 +113,7 @@ function wn_init(param)
     l.ifcProbe(ifAction).Add("ToggleVisibility", wn_togglevisibility);
     l.ifcProbe(ifAction).Add("Open", wn_openwin);
     l.ifcProbe(ifAction).Add("Close", wn_closewin);
+    l.ifcProbe(ifAction).Add("Popup", wn_popup);
 
     // Events
     var ie = l.ifcProbeAdd(ifEvent);
@@ -129,6 +132,7 @@ function wn_init(param)
     l.SetVisibilityBH = wn_setvisibility_bh;
     l.SetVisibilityTH = wn_setvisibility_th;
     l.Reveal = wn_cb_reveal;
+    l.wn_popup_bh = wn_popup_bh;
     pg_reveal_register_triggerer(l);
     pg_reveal_register_listener(l);
 
@@ -137,13 +141,93 @@ function wn_init(param)
     if (htr_getvisibility(l) == 'inherit' || htr_getvisibility(l) == 'visible')
 	{
 	l.is_visible = 1;
-	pg_addsched_fn(window, "pg_reveal_event", new Array(l,l,'Reveal'), 0);
+	pg_addsched_fn(window, "pg_reveal_event", [l,l,'Reveal'], 0);
 	}
 
     // Show container API
     l.showcontainer = wn_showcontainer;
 
     return l;
+    }
+
+// Popup - pops up a window in the way that a menu might pop up.
+function wn_popup(aparam)
+    {
+    var pop_to = null;
+    var pop_to_x = 0;
+    var pop_to_y = 0;
+    var pop_to_height = 0;
+    var pop_to_width = 0;
+
+    for (var w in wn_popped)
+	{
+	wn_popped[w].ifcProbe(ifAction).Invoke("Close", {});
+	}
+
+    if (aparam.PopTo)
+	{
+	if (wgtrIsNode(aparam.PopTo))
+	    pop_to = aparam.PopTo;
+	else 
+	    pop_to = wgtrGetNode(this, aparam.PopTo);
+	}
+    if (pop_to)
+	{
+	var geom = wgtrGetGeom(pop_to);
+	if (geom)
+	    {
+	    pop_to_x = geom.x;
+	    pop_to_y = geom.y;
+	    pop_to_width = geom.width;
+	    pop_to_height = geom.height;
+	    }
+	}
+    /*if (pop_to && (!pop_to.tagName || pop_to.tagName != 'DIV'))
+	{
+	// fixme -- not all widgets have a visual DIV
+	if (wgtrGetType(pop_to) == "widget/component")
+	    {
+	    var cmp_geom = pop_to.getGeom();
+	    pop_to_x = getPageX(wgtrGetContainer(wgtrGetParent(pop_to))) + cmp_geom.x;
+	    pop_to_y = getPageY(wgtrGetContainer(wgtrGetParent(pop_to))) + cmp_geom.y;
+	    pop_to_width = cmp_geom.width;
+	    pop_to_height = cmp_geom.height;
+	    }
+	pop_to = null;
+	}*/
+    if (aparam.X) pop_to_x = parseInt(aparam.X);
+    if (aparam.Y) pop_to_y = parseInt(aparam.Y);
+    if (aparam.Height) pop_to_height = parseInt(aparam.Height);
+    if (aparam.Width) pop_to_width = parseInt(aparam.Width);
+    /*if (pop_to)
+	{
+	pop_to_x = getPageX(pop_to);
+	pop_to_y = getPageY(pop_to);
+	pop_to_width = getClipWidth(pop_to);
+	pop_to_height = getClipHeight(pop_to);
+	}*/
+
+    if (aparam.OffsetX) pop_to_x += parseInt(aparam.OffsetX);
+    if (aparam.OffsetY) pop_to_y += parseInt(aparam.OffsetY);
+
+    if (pop_to_x || pop_to_y)
+	pg_positionpopup(this, pop_to_x, pop_to_y, pop_to_height, pop_to_width);
+
+    if (aparam.ExtendTo)
+	this.extended_region = wgtrGetGeom(aparam.ExtendTo);
+
+    this.popped_above = wgtrGlobalFindContainer(wgtrGetParent(this), "widget/childwindow");
+    if (this.popped_above)
+	this.popped_above.has_popup = this;
+
+    wn_popped[this.id] = this;
+    return this.ifcProbe(ifAction).Invoke('Open',aparam);
+    //pg_addsched_fn(this, 'wn_popup_bh', [aparam], 0);
+    }
+
+function wn_popup_bh(aparam)
+    {
+    return this.ifcProbe(ifAction).Invoke('Open',aparam);
     }
 
 // Called when we need to make sure the container is visible, typically
@@ -187,6 +271,8 @@ function wn_setvisibility_th(v)
 	pg_reveal_event(this, v, 'RevealCheck');
     else if (!v && (cur_vis == 'inherit' || cur_vis == 'visible'))
 	pg_reveal_event(this, v, 'ObscureCheck');
+    else if (v)
+	wn_bring_top(this);
     return;
     }
 
@@ -337,8 +423,19 @@ function wn_graphical_shade(l,to,speed,size)
 function wn_close(l)
     {
     if (l.is_modal) pg_setmodal(null);
+    if (wn_popped[l.id]) delete wn_popped[l.id];
     l.is_modal = false;
     l.no_close = false;
+    l.extended_region = null;
+    if (l.popped_above)
+	{
+	l.popped_above.has_popup = null;
+	l.popped_above = null;
+	}
+    if (l.has_popup)
+	{
+	wn_close(l.has_popup);
+	}
     if (l.closetype == 0 || !cx__capabilities.Dom0NS)
 	{
 	htr_setvisibility(l,'hidden');
@@ -501,11 +598,23 @@ function wn_bring_top(l)
     if (wn_topwin == l) return true;
     wn_adjust_z(l, wn_top_z - htr_getzindex(l) + 4);
     wn_topwin = l;
+    if (l.has_popup)
+	wn_bring_top(l.has_popup);
     }
 
 // FIXME: does this MOUSEDOWN work if for NS4 if there is no title?
 function wn_mousedown(e)
     {
+    for (var w in wn_popped)
+	{
+	var wn = wn_popped[w];
+	var pgx = getPageX(wn);
+	var pgy = getPageY(wn);
+	if ((!(e.pageX >= pgx && e.pageX < pgx + getClipWidth(wn) && e.pageY >= pgy && e.pageY < pgy + getClipHeight(wn))) && (!wn.extended_region || (!(e.pageX >= wn.extended_region.x && e.pageX < wn.extended_region.x + wn.extended_region.width && e.pageY >= wn.extended_region.y && e.pageY < wn.extended_region.y + wn.extended_region.width))))
+	    {
+	    wn.ifcProbe(ifAction).Invoke('SetVisibility',{IsVisible:0});
+	    }
+	}
     if (e.kind == 'wn')
         {
         if (e.target.name == 'close')
