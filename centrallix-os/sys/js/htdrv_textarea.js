ebe33d10836eed1d2883e32c5372e0a35b3cc256
gbeeley <gbeeley>
2010-09-17 01:20:53 +0000

    - (bugfix) a change to htdrv_form.js exposed a bug where the textarea's   setvalue() logic was expecting the textarea to be blank before it will   work reliably.

--- centrallix-os/sys/js/htdrv_textarea.js
+++ centrallix-os/sys/js/htdrv_textarea.js
@@ -24,6 +24,7 @@ function tx_getvalue()
 /** Set value function **/
 function tx_setvalue(txt)
     {
+    this.clearvalue();
     txt = htutil_obscure(txt);
     var reg = /(.*)(\n*)/;
     while(txt)
