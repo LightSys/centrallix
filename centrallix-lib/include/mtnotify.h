0df95918e2b9fb88405052b8e31cca2c5abf2334
Daniel Rothfus <danielrothfus@users.sourceforge.net>
2011-07-22 15:17:19 -0600

    Revised the API of mtNotification system
    This should be the final API

--- centrallix-lib/include/mtnotify.h
+++ centrallix-lib/include/mtnotify.h
@@ -1,27 +1,25 @@
 #ifndef MTNOTIFY_H
-#define	MTNOTIFY_H
+#define MTNOTIFY_H
+
 /************************************************************************/
-/* Centrallix Application Server System 				*/
-/* Centrallix Base Library						*/
-/* 									*/
-/* Copyright (C) 1998-2001 LightSys Technology Services, Inc.		*/
-/* 									*/
-/* You may use these files and this library under the terms of the	*/
-/* GNU Lesser General Public License, Version 2.1, contained in the	*/
-/* included file "COPYING".						*/
-/* 									*/
-/* Module: 	mtnotify.h      					*/
-/* Author:	Micah Shennum and Daniel Rothfus			*/
-/* Creation:	July 21, 2011   					*/
-/* Description:	A thread notification system that simplifies the        */
-/*              delivery of events to threads that are waiting on them.	*/
+/* Centrallix Application Server System                                 */
+/* Centrallix Base Library                                              */
+/*                                                                      */
+/* Copyright (C) 1998-2001 LightSys Technology Services, Inc.           */
+/*                                                                      */
+/* You may use these files and this library under the terms of the      */
+/* GNU Lesser General Public License, Version 2.1, contained in the     */
+/* included file "COPYING".                                             */
+/*                                                                      */
+/* Module:      mtnotify.h                                              */
+/* Author:      Micah Shennum and Daniel Rothfus                        */
+/* Creation:    July 21, 2011                                           */
+/* Description: A thread notification system that simplifies the        */
+/*              delivery of events to threads that are waiting on them. */
 /************************************************************************/
 
-#ifdef	__cplusplus
+#ifdef    __cplusplus
 extern "C" {
-#endif
-#ifdef SHOULDNOTBEDEFINED
-}
 #endif
 
 #ifdef CXLIB_INTERNAL
@@ -32,66 +30,92 @@ extern "C" {
 #include "cxlib/xhandle.h"
 #endif
 
-/** \addgroup mtn Notifcation System
- * \brief This system handles blocking notifications to threads.
- * \{
+/** @addgroup mtn Notifcation System
+ * @brief This system handles blocking notifications to threads.
+ * @{
  */
 
-/** \brief The type that is used for referencing the different kinds of events
- * that are passed by the notification system.
+/** @addgroup mtn_flags Notification Event Flags
+ * @brief All the valid flags that can be put into an Event structure.
+ * @{
  */
-typedef handle_t registered_event;
-
 #define ET_PHONESHOME    0
-#define ET_HEALS         0
-#define ET_BLOCKS        0
-#define ET_NOTBLOCKS     1
-#define ET_POLL         (1<<1)
+#define ET_HEALS         1
+/// @}
 
+/** @brief This is the structure that is passed all over when notfiying of events.
+ *
+ * These should always be created with mtnCreateEvent and deleted with mtnDeleteEvent
+ * whenever they are recieved from mtnWaitForEvents.
+ **/
 typedef struct _EV{
-    int flags;
-    /**@brief priority **/
-    int priority;
-    pXArray paramArray;
-    char *type;
+    /** @brief Do not modify - reference counting data for an event. */
+    unsigned short refcount;
+    /** @brief These are some of the ET_* options. */
+    int             flags;
+    /**@brief The priority of the event.
+     * The lower priority will trump those of higher values. 
+     **/
+    unsigned short   priority;
+    /** @brief The data you want to pass with the event.
+     *
+     * mtNotify makes no assumtions on the types of this data, the pointer will
+     * be completely unmodified, and will not be freed during mtnDeleteEvent.
+     **/
+    void            *param;
+    /** @brief Events are matched up from their sender to their receiver with this.
+     *
+     * There are a few disallowed names, specifically "quit" and "none".
+     **/
+    pXString        type;
 } Event, *pEvent;
 
 ///@brief Initializes the mtNotify system
-int mtnInitialize(int flags);
+int mtnInitialize();
 
-///@brief Cleans up after the mtNotify system and sends necessary events to quit
-/// threads that are waitng on events.
+/** @brief Cleans up after the mtNotify system and sends necessary events to quit
+ * threads that are waitng on events.
+ **/
 int mtnDeinitialize();
 
-/** \brief Create a new event of a given type.
- * \param typeOfEvent The type of event to create a new event of.
- * \return This returns the new event that was created.
+/** @brief Create a new event of a given type.
+ * @param typeOfEvent A string designating the type of event.
+ * @param data The data to pass with the event.  You are responsible for taking 
+ * care of the memory management of this data and knowing what type it is of.
+ * @param priority The lower this is, the more clout the event has.  0 is the 
+ * best that it can get.
+ * @param flags The flags for the event to affect delivery and such.
+ * @return This returns the new event that was created or NULL if the event could
+ * not be created.
  */
-pEvent mtnCreateEvent(char *typeOfEvent, int priority);
+pEvent mtnCreateEvent(pXString typeOfEvent, void * data, int priority, int flags);
 
-/** \brief Delete an event structure - NOTE THAT THIS SHOULD ONLY BE CALLED BY
- THE RECIEVER OF THE EVENT, NOT THE SENDER!
- \param event The event to delete.
+/** @brief Delete an event structure 
+ * @note NOTE THAT THIS SHOULD ONLY BE CALLED BY THE RECIEVER OF THE EVENT, NOT 
+ * THE SENDER! 
+ * @note ALSO, YOU MUST CALL THIS ON EVERY EVENT YOU RECIEVE FROM mtnWaitForEvent!
+ * @param event The event to delete.
  */
 void mtnDeleteEvent(pEvent event);
 
-/** \brief Send an event out to the necessary handler.
- * \param event The event to send out to some receiver.
+/** @brief Send an event out to the necessary handler.
+ * @param event The event to send out to some receiver.
  */
 int mtnSendEvent(pEvent event);
 
-/** \brief This is called by the thread that wants to receive the event when it
- happens.
- \param events The array of events to wait for.
- \return This returns the event that finally occurred.
- */
-pEvent mtnWaitForEvent(pXArray events);
+/** @brief This is called by the thread that wants to receive the event when it
+ * happens.
+ * @param eventStrings An array of strings of events to wait for.
+ * @param blocking If this call should block until there are meaningful return values.
+ * @return This returns the event that finally occurred.
+ * @TODO allow user to enter time to give up after
+ **/
+pEvent mtnWaitForEvents(pXArray eventStrings, int blocking, int prioity );
 
 ///\}
 
-#ifdef	__cplusplus
+#ifdef    __cplusplus
 }
 #endif
 
-#endif	/* MTNOTIFY_H */
-
+#endif    /* MTNOTIFY_H */
