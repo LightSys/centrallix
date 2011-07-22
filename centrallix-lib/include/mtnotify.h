d4a7268d26e5ea407229c61d43b1a513100045bf
Micah Shennum <jimtahu@gmail.com>
2011-07-22 10:03:26 -0600

    Initall commit of emerging interface
    new file:   centrallix-lib/include/mtnotify.h

---
+++ centrallix-lib/include/mtnotify.h
@@ -0,0 +1,101 @@
+#ifndef MTNOTIFY_H
+#define	MTNOTIFY_H
+/************************************************************************/
+/* Centrallix Application Server System 				*/
+/* Centrallix Base Library						*/
+/* 									*/
+/* Copyright (C) 1998-2001 LightSys Technology Services, Inc.		*/
+/* 									*/
+/* You may use these files and this library under the terms of the	*/
+/* GNU Lesser General Public License, Version 2.1, contained in the	*/
+/* included file "COPYING".						*/
+/* 									*/
+/* Module: 	mtnotify.h      					*/
+/* Author:	Micah Shennum and Daniel Rothfus			*/
+/* Creation:	July 21, 2011   					*/
+/* Description:	A thread notification system that simplifies the        */
+/*              delivery of events to threads that are waiting on them.	*/
+/************************************************************************/
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+#ifdef SHOULDNOTBEDEFINED
+}
+#endif
+
+#ifdef CXLIB_INTERNAL
+#include "xarray.h"
+#include "xhandle.h"
+#else
+#include "cxlib/xarray.h"
+#include "cxlib/xhandle.h"
+#endif
+
+/** \addgroup mtn Notifcation System
+ * \brief This system handles blocking notifications to threads.
+ * \{
+ */
+
+/** \brief The type that is used for referencing the different kinds of events
+ * that are passed by the notification system.
+ */
+typedef handle_t registered_event;
+
+#define ET_PHONESHOME   0
+#define ET_BLOCKS       1
+
+/// \brief One must fill out this event structure to get their event passed to
+/// them.
+typedef struct _ET{
+    int flags;
+    unsigned int numParams;
+} EventType, *pEventType;
+
+
+typedef struct _EV{
+    int flags;
+
+    pXArray paramArray;
+    pEventType type;
+} Event, *pEvent;
+
+///@brief Initializes the mtNotify system
+int mtnInitialize(int flags);
+
+///@brief Cleans up after the mtNotify system and sends necessary events to quit
+/// threads that are waitng on events.
+int mtnDeinitialize();
+
+/** \brief Create a new event of a given type.
+ * \param typeOfEvent The type of event to create a new event of.
+ * \return This returns the new event that was created.
+ */
+pEvent mtnCreateEvent(pEventType typeOfEvent);
+
+/** \brief Delete an event structure - NOTE THAT THIS SHOULD ONLY BE CALLED BY
+ THE RECIEVER OF THE EVENT, NOT THE SENDER!
+ \param event The event to delete.
+ */
+void mtnDeleteEvent(pEvent event);
+
+/** \brief Send an event out to the necessary handler.
+ * \param event The event to send out to some receiver.
+ */
+int mtnSendEvent(pEvent event, receiver sendTo);
+
+/** \brief This is called by the thread that wants to receive the event when it
+ happens.
+ \param events The array of events to wait for.
+ \return This returns the event that finally occurred.
+ */
+pEvent mtnWaitForEvent(receiver thisReceiver, pXArray events);
+
+///\}
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MTNOTIFY_H */
+
