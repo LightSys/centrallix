d83f6c1be139259a55d411efc3c9163b01bf1a29
Daniel Rothfus <danielrothfus@users.sourceforge.net>
2011-07-22 15:52:47 -0600

    Added outline for mtNotify framework

--- centrallix-lib/src/mtnotify.c
+++ centrallix-lib/src/mtnotify.c
@@ -16,21 +16,77 @@
 /************************************************************************/
 
 #include "mtnotify.h"
+#include "xhash.h"
+#include "newmalloc.h"
+#include <stdlib.h>
+
+volatile int running;
+pXArray currentlyWaiting; // List of semaphores waiting for events
+pEvent currentEvent;
+
+void mtn_internal_AddEventToQueue(pEvent event){
+    
+}
 
 int mtnInitialize(){
+    running = 1;
+    currentEvent = NULL;
+    currentlyWaiting = (pXArray)nmMalloc(sizeof(XArray));
+    xaInit(currentlyWaiting, 16);
 }
 
 int mtnDeinitialize(){
+    int numSemaphores;
+    
+    running = 0;
+    
+    // Clear all contents and drop all semaphores
+    numSemaphores = xaCount(currentlyWaiting);
+    while(numSemaphores--){
+        xaGetItem(currentlyWaiting, numSemaphores);
+    }
+    xaDeInit(currentlyWaiting);
+    nmFree(currentlyWaiting, sizeof(XArray));
 }
 
 pEvent mtnCreateEvent(pXString typeOfEvent, void * data, int priority, int flags){
+    
+    // Allocate structure
+    
+    // Allocate string
+    
 }
 
 void mtnDeleteEvent(pEvent event){
+    
+    // Free string
+    
+    // Free structure
+    
 }
 
 int mtnSendEvent(pEvent event){
+    
+    // Add into queue
+    
+    // Awake all semaphores
+    
 }
 
 pEvent mtnWaitForEvents(pXArray eventStrings, int blocking, int prioity ){
+    
+    // Create semaphore and add into array
+    
+    // While has not found an event
+    
+        // Wait for semaphore to break
+    
+        // Check if qualifies
+    
+            // If is not running, return NULL
+    
+            // If qualifies, remove semaphore from array and return that
+    
+            // If not, increment semaphore again
+    
 }
