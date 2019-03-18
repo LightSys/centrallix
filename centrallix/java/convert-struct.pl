45a13879d88f1aea1f5ad67ab552d9537d31bb5e
gubespam <gubespam@gmail.com>
2019-03-18 15:36:38 -0600

    auto-add imports

--- centrallix/java/convert-struct.pl
+++ centrallix/java/convert-struct.pl
@@ -4,68 +4,78 @@
 my $inStruct = 0;
 my $structName = 0;
 
-sub lcFirst {
-    my $a = shift;
-    return ucfirst $a;
-}
-
-my @lines = ();
+my @members = ();
 while(<>){
     chomp;
     if($inStruct){
         # array field
         if(/XArray[ \t]+([_a-zA-Z][_a-zA-Z0-9]*);/){
-            $fieldName = lcFirst($1);
-            push @lines, "    List get$fieldName();\n";
+            $fieldName = ucfirst($1);
+            push @members, "    List get$fieldName();\n";
+            push @imports, "import java.util.List;";
         }
         # string field
         elsif(/char[ \t]+([_a-zA-Z][_a-zA-Z0-9]*)\[(\d+)\];/){
-            $fieldName = lcFirst($1);
+            $fieldName = ucfirst($1);
             $size = $2;
-            push @lines, "    String get$fieldName(); // size $size\n";
+            push @members, "    String get$fieldName(); // size $size\n";
         }
         # regular field
         elsif(/([_a-zA-Z][_a-zA-Z0-9]*)[ \t]+([_a-zA-Z][_a-zA-Z0-9]*);/){
             $type = $1;
-            $fieldName = lcFirst($2);
-            push @lines, "    $type get$fieldName();\n";
+            $fieldName = ucfirst($2);
+            push @members, "    $type get$fieldName();\n";
         }
         # object field
         elsif(/void\*[ \t]+([_a-zA-Z][_a-zA-Z0-9]*);/){
-            $fieldName = lcFirst($1);
-            push @lines, "    Object $fieldName();\n";
+            $fieldName = ucfirst($1);
+            push @members, "    Object $fieldName();\n";
         }
         # object method
         elsif(/void\*[ \t]+\(\*([_a-zA-Z][_a-zA-Z0-9]*)\)\(\);/){
             $methodName = $1;
-            push @lines, "    Object $methodName();\n";
+            push @members, "    Object $methodName();\n";
         }
         # string method
         elsif(/char\*[ \t]+\(\*([_a-zA-Z][_a-zA-Z0-9]*)\)\(\);/){
             $methodName = $1;
-            push @lines, "    String $methodName();\n";
+            push @members, "    String $methodName();\n";
         }
         # regular method
         elsif(/([_a-zA-Z][_a-zA-Z0-9]*)[ \t]+\(\*([_a-zA-Z][_a-zA-Z0-9]*)\)\(\);/){
             $returnType = $1;
             $methodName = $2;
-            push @lines, "    $returnType $methodName();\n";
+            push @members, "    $returnType $methodName();\n";
         }
         elsif(/}/){
-            push @lines, "}\n\n";
         }
         elsif(/^[ \t]*([_a-zA-Z][_a-zA-Z0-9]*), .*;/){
             $structName = $1;
-            unshift @lines, "public interface $structName {\n";
-            for $line (@lines){
-                print $line;
-            }
-            $inStruct = 0;
             @lines = ();
+            push @lines, "package org.lightsys.centrallix.objectsystem;\n\n";
+            push @lines, @imports;
+            push @lines, "\n";
+            push @lines, "public interface $structName {\n";
+            push @lines, @members;
+            push @lines, "}\n";
+            dumpLines();
+            $inStruct = 0;
+            @members = ();
         }
     }
     elsif(/^typedef struct[ \t]+([__a-zA-Z][__a-zA-Z0-9]*)/){
         $inStruct = 1;
         $structName = $1;
     }
+}
+
+sub dumpLines {
+    my $name = $structName;
+    my $file = "src/main/java/org/lightsys/centrallix/objectsystem/$name.java";
+    print "Writing $file ...\n";
+    open $out, ">", $file or die "can't open file $file";
+    for $line (@lines){
+        print $out $line;
+    }
+    close $out;
 }
