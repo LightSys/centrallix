0fcc3d0af4df9ead98438367b933e336f7f88f79
Andrew Blomenberg <andrewBlomen@gmail.com>
2019-03-21 16:41:42 -0600

    First draft of .c files for chart widget
    The wgtdrv file is done as far as I know.
    In that file I was mainly adding virtual subwidgets for the axis and series.
    
    The htmlgen file is a complete rewrite.
    The code has been refactored to be very functional and clean.
    It's successfully passing some basic params through to the javascript and it should be easy to add more.

--- centrallix/htmlgen/htdrv_chart.c
+++ centrallix/htmlgen/htdrv_chart.c
@@ -12,54 +12,37 @@
 #include "cxlib/strtcpy.h"
 
 /************************************************************************/
-/* Centrallix Application Server System 				*/
-/* Centrallix Core       						*/
-/* 									*/
-/* Copyright (C) 1999-2007 LightSys Technology Services, Inc.		*/
-/* 									*/
-/* This program is free software; you can redistribute it and/or modify	*/
-/* it under the terms of the GNU General Public License as published by	*/
-/* the Free Software Foundation; either version 2 of the License, or	*/
-/* (at your option) any later version.					*/
-/* 									*/
-/* This program is distributed in the hope that it will be useful,	*/
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of	*/
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	*/
-/* GNU General Public License for more details.				*/
-/* 									*/
-/* You should have received a copy of the GNU General Public License	*/
-/* along with this program; if not, write to the Free Software		*/
-/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  		*/
-/* 02111-1307  USA							*/
-/*									*/
-/* A copy of the GNU General Public License has been included in this	*/
-/* distribution in the file "COPYING".					*/
-/*									*/
-/* Module: 	htdrv_table.c           				*/
-/* Author:	Greg Beeley (GRB)					*/
-/* Creation:	October 29, 1999 					*/
-/* Description:	HTML Widget driver for a data-driven table.  Has three	*/
-/*		different modes -- static, dynamicpage, and dynamicrow.	*/
-/*									*/
-/*		Static means an inline table that can't be updated	*/
-/*		without a parent container being completely reloaded.	*/
-/*		DynamicPage means a table in a layer that can be	*/
-/*		reloaded dynamically as a whole when necessary.  Good	*/
-/*		when you need forward/back without reloading the page.	*/
-/*		DynamicRow means each row is its own layer.  Good when	*/
-/*		you need to insert rows dynamically and delete rows	*/
-/*		dynamically at the client side without reloading the	*/
-/*		whole table contents.					*/
-/*									*/
-/*		A static table's query is performed on the server side	*/
-/*		and the HTML is generated at the server.  Both dynamic	*/
-/*		types are built from a client-side query.  Static 	*/
-/*		tables are generally best when the data will be read-	*/
-/*		only.  Dynamicrow tables use the most client resources.	*/
+/* Centrallix Application Server System                                 */
+/* Centrallix Core                                                      */
+/*                                                                      */
+/* Copyright (C) 1999-2007 LightSys Technology Services, Inc.           */
+/*                                                                      */
+/* This program is free software; you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* This program is distributed in the hope that it will be useful,      */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with this program; if not, write to the Free Software          */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA             */
+/* 02111-1307  USA                                                      */
+/*                                                                      */
+/* A copy of the GNU General Public License has been included in this   */
+/* distribution in the file "COPYING".                                  */
+/*                                                                      */
+/* Module:      htdrv_chart.c                                           */
+/* Author:      Andrew Blomenberg                                       */
+/* Creation:    March 19, 2019                                          */
+/* Description:                                                         */
 /************************************************************************/
 
 
-#define HTTBL_MAX_COLS		(32)
+#define HTTBL_MAX_COLS (32)
 
 /** globals **/
 static struct
@@ -67,150 +50,305 @@ static struct
     int idcnt;
 } HTCHT;
 
-
-typedef struct
-{
-    char name[64];
-    char canvas_id[64];
-    char sbuf[160];
-    char cht_bgnd[128];
-    char hdr_bgnd[128];
-    char row_bgnd1[128];
-    char row_bgnd2[128];
-    char row_bgndhigh[128];
-    char colsep_bgnd[128];
-    char textcolor[64];
-    char textcolorhighlight[64];
-    char titlecolor[64];
-    char newrow_bgnd[128];
-    char newrow_textcolor[64];
-    char osrc[64];
-    char row_border[64];
-    char row_shadow_color[64];
-    int row_shadow;
-    int row_shadow_radius;
-    int row_radius;
-    int x,y,w,h;
-    int data_mode;		/* 0="rows" or 1="properties" */
-    int outer_border;
-    int inner_border;
-    int inner_padding;
-    int ncols;
-    int windowsize;
-    int min_rowheight;
-    int max_rowheight;
-    int cellhspacing;
-    int cellvspacing;
-    int followcurrent;
-    int dragcols;
-    int colsep;
-    int gridinemptyrows;
-    int allow_selection;
-    int show_selection;
-    int initial_selection;
-    int reverse_order;
-    int overlap_scrollbar;	/* scrollbar overlaps with table */
-    int hide_scrollbar;		/* don't show scrollbar at all */
-    int demand_scrollbar;	/* only show scrollbar when needed */
-    int has_header;		/* table has header/title row? */
-    int rowcache_size;		/* number of rows the table caches for display */
-} htcht_struct;
-
 int
-htchtRenderDynamic(pHtSession s, pWgtrNode tree, int z, htcht_struct* c)
+htchtCheckBrowserSupport(pHtSession session)
     {
-        if(!s->Capabilities.Dom0NS && !s->Capabilities.Dom1HTML)
-        {
-            mssError(1,"HTTBL","Netscape 4 DOM or W3C DOM support required");
+        if (!session->Capabilities.Dom0NS && !session->Capabilities.Dom1HTML) {
+            mssError(1, "HTTBL", "Netscape 4 DOM or W3C DOM support required");
             return -1;
         }
-
-        /** STYLE for the layer **/
-        htrAddScriptInclude(s, "/sys/js/htdrv_chart.js", 0);
-        htrAddScriptInclude(s, "/sys/js/chartjs/Chart.js", 0);
-        htrAddScriptInclude(s, "/sys/js/ht_utils_string.js", 0);
-
-        htrAddScriptInit_va(s,"    cht_init({"
-                                  "osrc: %['%STR&SYM'%]%[null%],"
-                                  "chart: wgtrGetNodeRef(ns,\"%STR&SYM\"),"
-                                  "width: %POS,"
-                                  "canvas_id: '%STR&SYM'"
-                              "});\n",
-                              *(c->osrc) != '\0', c->osrc, *(c->osrc) == '\0',
-                              c->name,
-                              c->w,
-                              c->canvas_id
-        );
-
-        htrAddBodyItem_va(s,"<DIV> <CANVAS ID=\"%STR&SYM\">\n",c->canvas_id);
-        htrAddBodyItem(s,"<P>CHART HERE</P>\n");
-        htrAddBodyItem(s,"</CANVAS> </DIV>\n");
-
         return 0;
     }
 
 
-
-/*** htchtRender - generate the HTML code for the page.
- ***/
 int
-htchtRender(pHtSession s, pWgtrNode tree, int z)
+htchtGetIntValue(pWgtrNode tree, char* prop_name, int default_val)
+    {
+    int out_val;
+        if (wgtrGetPropertyValue(tree,prop_name,DATA_T_INTEGER,POD(&out_val)) != 0)
+            return default_val;
+        else
+            return out_val;
+    }
+
+
+int
+htchtGetStrValue(pWgtrNode tree, char* prop_name, char* default_val, char* buf)
+    {
+    int rval;
+    char* ptr;
+
+        /** We're not passing the buf into this function call because of convoluted c reasons **/
+        rval = wgtrGetPropertyValue(tree,prop_name,DATA_T_STRING,POD(&ptr));
+
+        if(rval == 0)
+            {
+            strtcpy(buf, ptr, sizeof(buf));
+            return 0;
+            }
+        else
+            {
+            strtcpy(buf, default_val, sizeof(buf));
+            return -1;
+            }
+    }
+
+
+int
+htchtGetWidth(pWgtrNode tree){return htchtGetIntValue(tree, "w", -1);}
+
+int
+htchtGetX(pWgtrNode tree){return htchtGetIntValue(tree, "x", -1);}
+
+int
+htchtGetY(pWgtrNode tree){return htchtGetIntValue(tree, "y", -1);}
+
+int
+htchtGetHeight(pWgtrNode tree)
+    {
+    int height;
+        height = htchtGetIntValue(tree, "height", -1);
+        if (height == -1)
+            {
+            mssError(1, "HTCHT", "'height' property is required");
+            return -1;
+            }
+
+        return height;
+    }
+
+int
+htchtGetName(pWgtrNode tree, char* buf) {return htchtGetStrValue(tree, "name", "", buf);}
+
+int
+htchtGetType(pWgtrNode tree, char* buf)
+    {
+    char chart_types[16][16] = {"line", "bar"};
+    int rval;
+    char err_msg[256];
+    int found = 0;
+    int type_idx;
+
+        rval = htchtGetStrValue(tree, "chart_type", "", buf);
+
+        if (rval != 0)
+            return rval;
+
+        sprintf(err_msg, "%s is not a valid chart type. Supported types are: ", buf);
+
+        for (type_idx = 0; type_idx < 16; type_idx++)
+            {
+            if (strcmp(buf, chart_types[type_idx]) == 0)
+                found = 1;
+
+            strcat(err_msg, chart_types[type_idx]);
+            strcat(err_msg, ", ");
+            }
+
+        if (found) return 0;
+        else
+            {
+            mssError(1, "HTCHT", err_msg);
+            strcpy(buf, "");
+            return -1;
+            }
+    }
+
+int
+htchtGetTextColor(pWgtrNode tree, char* buf) {return htchtGetStrValue(tree, "textcolor", "", buf);}
+
+int
+htchtGetObjectSource(pWgtrNode tree, char* buf)
+    {
+        /**  The ObjectSource can be provided in the configuration for this widget,
+         **  but it is usually determined by the javascript driver.
+         **  Setting ObjectSource to an empty string indicates that the javascript should determine the ObjectSource.
+         **/
+        return htchtGetStrValue(tree, "objectsource", "", buf);
+    }
+
+void
+htchtGetCanvasId(pWgtrNode tree, char* buf){sprintf(buf, "cht%dcanvas", HTCHT.idcnt);}
+
+void
+htchtGetSeriesProperties(pHtSession session, pWgtrNode tree, char* buf)
+    {
+    int num_series;
+    int series_idx;
+    pWgtrNode series[32];
+    pWgtrNode sub_tree;
+    char series_object[256];
+
+    char label[32];
+    char color[32];
+
+        buf[0] = '\0';
+        strcat(buf, "[");
+
+        num_series = wgtrGetMatchingChildList(tree, "widget/chart-series", series, sizeof(series)/sizeof(pWgtrNode));
+        for (series_idx = 0; series_idx < num_series; series_idx++)
+            {
+           sub_tree = series[series_idx];
+
+            /** It is actually possible for the subwidgets to be in another namespace, so we check that **/
+            htrCheckNSTransition(session, tree, sub_tree);
+
+            htchtGetStrValue(sub_tree, "label", "", label);
+            htchtGetStrValue(sub_tree, "color", "", color);
+
+            htrCheckNSTransitionReturn(session, tree, sub_tree);
+
+            sprintf(series_object, "{"
+                                       "label: \"%s\", "
+                                       "color: \"%s\""
+                                   "},",
+                                   label,
+                                   color);
+
+            strcat(buf, series_object);
+            }
+        strcat(buf, "]");
+
+        return;
+    }
+
+
+void
+htchtGetAxesProperties(pHtSession session, pWgtrNode tree, char* buf)
+    {
+    int num_axes;
+    int axes_idx;
+    pWgtrNode axes[32];
+    pWgtrNode sub_tree;
+    char axis_object[256];
+
+    char label[32];
+    int bold;
+
+        buf[0] = '\0';
+        strcat(buf, "[");
+
+        num_axes = wgtrGetMatchingChildList(tree, "widget/chart-axis", axes, sizeof(axes)/sizeof(pWgtrNode));
+        for (axes_idx = 0; axes_idx < num_axes; axes_idx++)
+            {
+                sub_tree = axes[axes_idx];
+
+                /** It is actually possible for the subwidgets to be in another namespace, so we check that **/
+                htrCheckNSTransition(session, tree, sub_tree);
+
+                htchtGetStrValue(sub_tree, "label", "", label);
+                bold = htrGetBoolean(tree, "bold", 0);
+
+                htrCheckNSTransitionReturn(session, tree, sub_tree);
+
+                sprintf(axis_object, "{"
+                                     "label: \"%s\", "
+                                       "bold: \"%d\""
+                                       "},",
+                        label,
+                        bold);
+
+                strcat(buf, axis_object);
+            }
+        strcat(buf, "]");
+
+        return;
+    }
+
+
+
+void
+htchtInitCall(pHtSession session, pWgtrNode tree)
+    {
+    char object_source[64];
+    char name[64];
+    char chart_type[64];
+    char canvas_id[64];
+    char text_color[64];
+    char axes_properties[2048];
+    char series_properties[2048];
+
+        htchtGetObjectSource(tree, object_source);
+        htchtGetName(tree, name);
+        htchtGetType(tree, chart_type);
+        htchtGetCanvasId(tree, canvas_id);
+        htchtGetTextColor(tree, text_color);
+        htchtGetAxesProperties(session, tree, axes_properties);
+        htchtGetSeriesProperties(session, tree, series_properties);
+
+        htrAddScriptInit_va(session, "    "
+                "cht_init({"
+                    "x_pos: %INT,"
+                    "y_pos: %INT,"
+                    "width: %INT,"
+                    "height: %INT,"
+                    "chart: wgtrGetNodeRef(ns,\"%STR&SYM\"),"
+                    "chart_type: '%STR',"
+                    "canvas_id: '%STR&SYM',"
+                    "osrc: '%STR',"
+                    "text_color: '%STR',"
+                    "axes: %STR,"
+                    "series: %STR"
+                "});\n",
+                    htchtGetX(tree),
+                    htchtGetY(tree),
+                    htchtGetWidth(tree),
+                    htchtGetHeight(tree),
+                    name,
+                    chart_type,
+                    canvas_id,
+                    object_source,
+                    text_color,
+                    axes_properties,
+                    series_properties
+        );
+    }
+
+
+void
+htchtScriptInclude(pHtSession session)
+    {
+        htrAddScriptInclude(session, "/sys/js/htdrv_chart.js", 0);
+        htrAddScriptInclude(session, "/sys/js/chartjs/Chart.js", 0);
+        htrAddScriptInclude(session, "/sys/js/ht_utils_string.js", 0);
+    }
+
+
+void
+htchtGenHTML(pHtSession session, pWgtrNode tree)
+    {
+    char buf[32];
+        htchtGetCanvasId(tree, buf);
+
+        htrAddBodyItem_va(session,"<DIV> <CANVAS ID=\"%STR&SYM\">\n", buf);
+        htrAddBodyItem(session,"<P>CHART HERE</P>\n");
+        htrAddBodyItem(session,"</CANVAS> </DIV>\n");
+    }
+
+int
+htchtRender(pHtSession session, pWgtrNode tree, int z)
     {
     char* ptr;
-    int rval;
-    htcht_struct* chart;
+    char buf[32];
 
-        /** Don't try to render table-column, etc.  We do that elsewhere **/
         wgtrGetPropertyValue(tree,"outer_type",DATA_T_STRING,POD(&ptr));
         if (strcmp(ptr, "widget/chart") != 0)
             return 0;
 
-        chart = (htcht_struct*)nmMalloc(sizeof(htcht_struct));
-        if (!chart) return -1;
+        if (htchtCheckBrowserSupport(session) != 0) return -1;
 
-        memset(chart, 0, sizeof(htcht_struct));
+        /** Verify that certain parameters are provided **/
+        if (htchtGetHeight(tree) == -1) return -1;
+        if (htchtGetName(tree, buf) != 0) return -1;
 
-        chart->x=-1;
-        chart->y=-1;
+        htchtGenHTML(session, tree);
+        htchtScriptInclude(session);
+        htchtInitCall(session, tree);
 
-        /** Get an id for that. **/
-        sprintf(chart->canvas_id, "cht%dcanvas", HTCHT.idcnt++);
+        /** Set the ID for the next chart **/
+        HTCHT.idcnt++;
 
-        /** Get x,y,w,h of this object **/
-        if (wgtrGetPropertyValue(tree,"x",DATA_T_INTEGER,POD(&(chart->x))) != 0) chart->x = -1;
-        if (wgtrGetPropertyValue(tree,"y",DATA_T_INTEGER,POD(&(chart->y))) != 0) chart->y = -1;
-        if (wgtrGetPropertyValue(tree,"width",DATA_T_INTEGER,POD(&(chart->w))) != 0) chart->w = -1;
-        if (wgtrGetPropertyValue(tree,"height",DATA_T_INTEGER,POD(&(chart->h))) != 0)
-        {
-            mssError(1,"HTTBL","'height' property is required");
-            return -1;
-        }
-
-        /** Get name **/
-        if (wgtrGetPropertyValue(tree,"name",DATA_T_STRING,POD(&ptr)) != 0)
-        {
-            nmFree(chart, sizeof(htcht_struct));
-            return -1;
-        }
-
-        strtcpy(chart->name,ptr,sizeof(chart->name));
-
-        if (wgtrGetPropertyValue(tree,"objectsource",DATA_T_STRING,POD(&ptr)) == 0)
-            strtcpy(chart->osrc,ptr,sizeof(chart->osrc));
-        else
-            strcpy(chart->osrc,"");
-
-//        chart->ncols = wgtrGetPropertyValue(tree,"objectsource",DATA_T_STRING,POD(&ptr));
-
-        /** Text color information **/
-        if (wgtrGetPropertyValue(tree,"textcolor",DATA_T_STRING,POD(&ptr)) == 0)
-            strtcpy(chart->textcolor,ptr,sizeof(chart->textcolor));
-
-        rval = htchtRenderDynamic(s, tree, z, chart);
-
-        nmFree(chart, sizeof(htcht_struct));
-
-        return rval;
+        return 0;
     }
 
 
@@ -229,6 +367,8 @@ htchtInitialize()
         strcpy(drv->Name,"DHTML Chart Driver");
         strcpy(drv->WidgetName,"chart");
         drv->Render = htchtRender;
+        xaAddItem(&(drv->PseudoTypes), "chart-axis");
+        xaAddItem(&(drv->PseudoTypes), "chart-series");
 
         htrAddEvent(drv,"Click");
         htrAddEvent(drv,"DblClick");
--- centrallix/wgtr/wgtdrv_chart.c
+++ centrallix/wgtr/wgtdrv_chart.c
@@ -7,33 +7,33 @@
 #include "wgtr.h"
 
 /************************************************************************/
-/* Centrallix Application Server System 				*/
-/* Centrallix Core       						*/
-/* 									*/
-/* Copyright (C) 1998-2008 LightSys Technology Services, Inc.		*/
-/* 									*/
-/* This program is free software; you can redistribute it and/or modify	*/
-/* it under the terms of the GNU General Public License as published by	*/
-/* the Free Software Foundation; either version 2 of the License, or	*/
-/* (at your option) any later version.					*/
-/* 									*/
-/* This program is distributed in the hope that it will be useful,	*/
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of	*/
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	*/
-/* GNU General Public License for more details.				*/
-/* 									*/
-/* You should have received a copy of the GNU General Public License	*/
-/* along with this program; if not, write to the Free Software		*/
-/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  		*/
-/* 02111-1307  USA							*/
-/*									*/
-/* A copy of the GNU General Public License has been included in this	*/
-/* distribution in the file "COPYING".					*/
-/* 									*/
-/* Module: 	wgtr/wgtdrv_chart.c					*/
-/* Author:	Andrew Blomenberg		 			*/
-/* Creation:	March 18, 2019						*/
-/* Description:								*/
+/* Centrallix Application Server System                                 */
+/* Centrallix Core                                                      */
+/*                                                                      */
+/* Copyright (C) 1998-2008 LightSys Technology Services, Inc.           */
+/*                                                                      */
+/* This program is free software; you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License, or    */
+/* (at your option) any later version.                                  */
+/*                                                                      */
+/* This program is distributed in the hope that it will be useful,      */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        */
+/* GNU General Public License for more details.                         */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with this program; if not, write to the Free Software          */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA             */
+/* 02111-1307  USA                                                      */
+/*                                                                      */
+/* A copy of the GNU General Public License has been included in this   */
+/* distribution in the file "COPYING".                                  */
+/*                                                                      */
+/* Module:     wgtr/wgtdrv_chart.c                                      */
+/* Author:    Andrew Blomenberg                                         */
+/* Creation:    March 18, 2019                                          */
+/* Description:                                                         */
 /************************************************************************/
 
 
@@ -57,15 +57,21 @@ wgtchtVerify(pWgtrVerifySession s)
  ***/
 int
 wgtchtNew(pWgtrNode node)
-    {   
-
-	if (!strcmp(node->Type, "widget/chart"))
-	    {
-	    if(node->fl_width < 0) node->fl_width = 100;
-	    if(node->fl_height < 0) node->fl_height = 100;
-	    }
-	
-    return 0;
+    {
+        if (!strcmp(node->Type, "widget/chart"))
+        {
+            if(node->fl_width < 0) node->fl_width = 100;
+            if(node->fl_height < 0) node->fl_height = 100;
+        }
+        else if (!strcmp(node->Type, "widget/chart-axis"))
+        {
+            node->Flags |= WGTR_F_NONVISUAL;
+        }
+        else if (!strcmp(node->Type, "widget/chart-series"))
+        {
+            node->Flags |= WGTR_F_NONVISUAL;
+        }
+        return 0;
     }
 
 
@@ -74,8 +80,10 @@ wgtchtInitialize()
     {
     char* name = "Chart Driver";
 
-	wgtrRegisterDriver(name, wgtchtVerify, wgtchtNew);
-	wgtrAddType(name, "chart");
+    wgtrRegisterDriver(name, wgtchtVerify, wgtchtNew);
+    wgtrAddType(name, "chart");
+    wgtrAddType(name, "chart-series");
+    wgtrAddType(name, "chart-axis");
 
-	return 0;
+    return 0;
     }
