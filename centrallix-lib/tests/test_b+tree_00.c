8fa2cf10ebf65eaf5da4f980d61f05c6bffe1264
Aisha Terrill <Aisha.Terrill@LightSys.org>
2020-05-21 11:54:52 -0500

    Updated makefile to include b+tree

--- centrallix-lib/tests/test_b+tree_00.c
+++ centrallix-lib/tests/test_b+tree_00.c
@@ -2,11 +2,6 @@
 #include <assert.h>
 #include "b+tree.h"
 
-// edit Makefile.in
-// ./configure
-// export TONLY=b+tree
-// make test 
-
 long long
 test(char** tname)
     {
@@ -14,18 +9,15 @@ test(char** tname)
     int iter;
 	int tmp;
 
-	*tname = "b+tree-00 bptNew <test something> and printf";
+	*tname = "b+tree-00 CURRENTLY A DUMMY TEST";
 	iter = 800000;
 	for(i=0;i<iter;i++)
 	    {
-	    //pBPTree tree = NULL;
-		//tree = bptNew();
-		//assert (tree != NULL);
 		tmp = bpt_i_Push(NULL);
 		assert (tmp == 0);
 	    }
 
-	//printf("\nhello world\n\n");
+	printf("\nhello world\n\n");
 
     return iter*4;
     }
