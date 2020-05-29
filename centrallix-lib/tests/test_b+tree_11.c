9e276472ae7bc7e501e2aa16fc1e62c2ad0fad56
Aisha Terrill <Aisha.Terrill@LightSys.org>
2020-05-29 11:57:58 -0500

    Added tests for bptDeInit

---
+++ centrallix-lib/tests/test_b+tree_11.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "b+tree.h"
+
+long long
+test(char** tname)
+    {
+    int i; 
+    int iter;
+	pBPTree tree;
+	int val;
+
+	*tname = "b+tree-11 bptDeInit works for leaf tree with 3 values";
+
+	iter = 800000;
+	for(i=0;i<iter;i++)
+	 	{
+		tree = bptNew();
+		bptInit(tree);
+		tree->Keys[0].Length = 2;
+		tree->Keys[0].Value = nmSysMalloc(2); // don't assign double quoted string because deInit frees this
+		tree->Keys[1].Length = 5;
+		tree->Keys[1].Value = nmSysMalloc(5);
+		tree->Keys[2].Length = 3;
+		tree->Keys[2].Value = nmSysMalloc(3);
+		tree->nKeys = 3;
+		tree->IsLeaf = 1;
+
+		val = bptDeInit(tree);
+		assert (val == 0);
+		assert (tree->Parent == NULL);
+		assert (tree->Next == NULL);
+		assert (tree->Prev == NULL);
+		assert (tree->nKeys == 0);		
+		}
+    return iter;
+    }
---
+++ centrallix-lib/tests/test_b+tree_14.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "b+tree.h"
+#include "newmalloc.h"
+
+#define NOPRINT
+#ifdef PRINT
+	#define PRINTF(f_, ...) printf((f_), ##__VA_ARGS__)
+#else
+	#define PRINTF(f_, ...) ;
+#endif
+
+long long
+test(char** tname)
+    {
+    int i,j,k,m; 
+    int iter;
+	int NUMLEVELS = 1;
+	int LEVELNKEYS[] = {10, 10, 10, 10}; // all nodes in same level have same # of keys
+	int LEVELNNODES[NUMLEVELS];
+	LEVELNNODES[0] = 1;
+	for (i=1; i<NUMLEVELS; i++) LEVELNNODES[i] = (LEVELNKEYS[i-1]+1)*(LEVELNNODES[i-1]);
+	int TOTNODES = 0;
+	for (i=0; i<NUMLEVELS; i++) TOTNODES += LEVELNNODES[i];
+	int KEYLEN = 5;
+	int val;
+
+	*tname = "b+tree-14 bptDeInit works for large tree (configurable)";
+
+	iter = 80000;
+	for(i=0;i<iter;i++)
+	 	{
+		pBPTree nodes[TOTNODES];
+
+		int nodeIndex = 0;
+		int parentIndex = 0;
+
+		// do all except assigning children, next, prev
+		for (j=0; j<NUMLEVELS; j++)
+			{
+			for (k=0; k<LEVELNNODES[j]; k++)
+				{
+				nodes[nodeIndex] = bptNew();
+				bptInit(nodes[nodeIndex]);
+				nodes[nodeIndex]->nKeys = LEVELNKEYS[j];
+				nodes[nodeIndex]->IsLeaf = (j==NUMLEVELS-1);
+				PRINTF("%d:#%d", nodeIndex, nodes[nodeIndex]->nKeys);				
+				for (m=0; m<LEVELNKEYS[j]; m++)
+					{
+					nodes[nodeIndex]->Keys[m].Length = KEYLEN;
+					nodes[nodeIndex]->Keys[m].Value = nmSysMalloc(KEYLEN);
+					}
+				
+				if (j!=0)
+					{
+					PRINTF(":p%d", parentIndex);
+					nodes[nodeIndex]->Parent = nodes[parentIndex];
+					if ((k+1)%(LEVELNKEYS[j-1]+1)==0) parentIndex++;
+					}
+				PRINTF("\t");
+			
+				nodeIndex++;
+				}
+				PRINTF("\n");
+			}
+
+		int childIndex = 1;
+		nodeIndex = 0;
+		
+		// assign children, next, prev
+		for (j=0; j<NUMLEVELS; j++)
+			{
+			for (k=0; k<LEVELNNODES[j]; k++)
+				{				
+				if (!nodes[nodeIndex]->IsLeaf)
+					{
+					for (m=0; m<=LEVELNKEYS[j]; m++)
+						{
+						nodes[nodeIndex]->Children[m].Child = nodes[childIndex++];
+						}
+					}
+
+				if (k>0) nodes[nodeIndex]->Prev = nodes[nodeIndex-1];
+				if (j!=0 && k<(LEVELNKEYS[j]*(LEVELNKEYS[j-1]+1)-1)) nodes[nodeIndex]->Next = nodes[nodeIndex+1];
+				nodeIndex++;
+				}
+			}
+
+		val = bptDeInit(nodes[0]);
+		assert (val == 0);
+		for (j=0; j<TOTNODES; j++)
+			{
+			assert (nodes[j]->Parent == NULL);
+			assert (nodes[j]->Next == NULL);
+			assert (nodes[j]->Prev == NULL);
+			assert (nodes[j]->nKeys == 0);	
+			}
+		}
+    return iter;
+    }
+
+
+
