ec099435a32989dd608df7d64e12c74e73581ad9
Developer - Kardia <ttobrien@crimson.ua.edu>
2020-07-11 16:35:32 -0600

    switched howto.htm to UTF-8

--- centrallix-lib/tests/test_xstring_00.c
+++ centrallix-lib/tests/test_xstring_00.c
@@ -4,23 +4,33 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "../../centrallix/include/charsets.h"
+#include <locale.h>
+#include "xstring.h"
 
 long long
 test(char** tname)
     {
-    int i;
-    int iter;
-    size_t len;
+    	*tname = "Test chrCharLength Function";
+	int i, iter, len;
+	setlocale(LC_ALL, "en_US.UTF-8");
+	
+	iter = 100000;
+        for(i=0;i<iter;i++)
+		{
+		len = chrCharLength("ABC\0");
+        	assert(len == 3);
 
-	char* str2 = "Χαίρετε";
-	len = chrCharLength(str2);
-	printf("num chars = %d", len);
-	assert(len == 7);
+		len = chrCharLength("他复活了！\0");
+        	assert(len == 5);	
 
-	*tname = "Accessing centrallix";
-	iter = 10000000;
-	for(i=0;i<iter;i++) assert (0 == 0);
+		len = chrCharLength("پرکھ\0");
+		assert(len == 4);
+
+		len = chrCharLength("イエスは主です\0");
+		assert(len == 7);
+
+		
+		}
 
     return iter;
     }
