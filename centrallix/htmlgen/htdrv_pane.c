5accf926b19baaf44100862f08ad195427e15da1
CarlYunghans <64278930+CarlYunghans@users.noreply.github.com>
2020-07-15 14:01:32 -0400

    Remedied another error from two commits ago
    The function isMainContainer() was used before it was declared.

--- centrallix/htmlgen/htdrv_pane.c
+++ centrallix/htmlgen/htdrv_pane.c
@@ -50,6 +50,29 @@ static struct
     HTPN;
 
 
+bool
+isMainContainer(pWgtrNode stranger) //Function to determine whether stranger is a main container - a widget whose width spans the page and whose immediate children are smaller than it
+   {
+	pWgtrNode theWholeShebang = wgtrGetRoot(stranger); //Lay hold of the top level of stranger's tree
+	int shebangWidth; //Variable to store top-level width
+	int mandatoryInt = wgtrGetPropertyValue(theWholeShebang,"width",DATA_T_STRING,POD(&shebangWidth)); //Load top-level width into shebangWidth (mandatoryInt is necessary but unrelated to the logic)
+	int strangerWidth; //Variable to store stranger's width
+	mandatoryInt = wgtrGetPropertyValue(stranger,"width",DATA_T_STRING,POD(&strangerWidth)); //Load stranger's width into strangerWidth
+	pWgtrNode strangerFirstborn = xaGetItem(&(stranger->Children), 0); //Make like Rumplestiltskin and seize the first child
+	int firstbornWidth; //Variable to store first child's width
+	mandatoryInt = wgtrGetPropertyValue(strangerFirstborn,"width",DATA_T_STRING,POD(&firstbornWidth)); //Measure him side to side
+	
+	if (strangerWidth >= (shebangWidth - 29)) //If stranger's width spans the whole tree... (allow for margins and/or padding - the smallest possible widgets are 30px wide)
+	    {
+		if (firstbornWidth <= strangerWidth - 29)//And his first child is thinner than he... (once again, allowing for margins and/or padding)
+		    {
+			return 1; //We have ourselves a winner!
+		    }
+	    }
+	return 0; //Nope.
+   }
+   
+
 /*** htpnRender - generate the HTML code for the page.
  ***/
 int
@@ -284,28 +307,6 @@ htpnRender(pHtSession s, pWgtrNode tree, int z)
 
     return 0;
     }
-
-bool
-isMainContainer(pWgtrNode stranger) //Function to determine whether stranger is a main container - a widget whose width spans the page and whose immediate children are smaller than it
-   {
-	pWgtrNode theWholeShebang = wgtrGetRoot(stranger); //Lay hold of the top level of stranger's tree
-	int shebangWidth; //Variable to store top-level width
-	int mandatoryInt = wgtrGetPropertyValue(theWholeShebang,"width",DATA_T_STRING,POD(&shebangWidth)); //Load top-level width into shebangWidth (mandatoryInt is necessary but unrelated to the logic)
-	int strangerWidth; //Variable to store stranger's width
-	mandatoryInt = wgtrGetPropertyValue(stranger,"width",DATA_T_STRING,POD(&strangerWidth)); //Load stranger's width into strangerWidth
-	pWgtrNode strangerFirstborn = xaGetItem(&(stranger->Children), 0); //Make like Rumplestiltskin and seize the first child
-	int firstbornWidth; //Variable to store first child's width
-	mandatoryInt = wgtrGetPropertyValue(strangerFirstborn,"width",DATA_T_STRING,POD(&firstbornWidth)); //Measure him side to side
-	
-	if (strangerWidth >= (shebangWidth - 29)) //If stranger's width spans the whole tree... (allow for margins and/or padding - the smallest possible widgets are 30px wide)
-	    {
-		if (firstbornWidth <= strangerWidth - 29)//And his first child is thinner than he... (once again, allowing for margins and/or padding)
-		    {
-			return 1; //We have ourselves a winner!
-		    }
-	    }
-	return 0; //Nope.
-   }
 
 /*** htpnInitialize - register with the ht_render module.
  ***/
