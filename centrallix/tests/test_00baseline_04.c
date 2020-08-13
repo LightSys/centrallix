0af24fb32de7af375be379fa0bb36d42872ba182
sheesania <sheesania@users.noreply.github.com>
2020-08-13 15:35:02 -0600

    Remove extraneous name comments from old native C baselines

--- centrallix/tests/test_00baseline_04.c
+++ centrallix/tests/test_00baseline_04.c
@@ -1,8 +1,6 @@
-//NAME Native C ** SHOULD Pass **
-
 long long
 test(char** name)
     {
-    *name = "Native C ** SHOULD Pass **";
+    *name = "00baseline_04 Native C ** SHOULD Pass **";
     return 0;
     }
