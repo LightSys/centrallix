875da6e510ff50c315237f935320048970279480
Carl Cederborg <45981643+cscederborg@users.noreply.github.com>
2020-08-18 10:16:09 -0600

    Initial MoneyType Tests for obj_datatype functions

--- centrallix/tests/test_moneytype_example.c
+++ centrallix/tests/test_moneytype_example.c
@@ -5,7 +5,9 @@ long long
 test(char** name)
 {
     *name = "MoneyType example";
-    MoneyType test = {1, 1};
+    
+    MoneyType test = {10000};
     assert(strcmp(objFormatMoneyTmp(&test, NULL), "$1.00") == 0);
+    
     return 0;
 }
