1515d913384e1e8915c71ff692640071d57176c3
Carl Cederborg <45981643+cscederborg@users.noreply.github.com>
2020-08-18 10:41:12 -0600

    MoneyType Tests for obj_datatype functions; Removal of Example

---
+++ centrallix/tests/test_moneytype_00.c
@@ -0,0 +1,30 @@
+#include "obj.h"
+#include <assert.h>
+
+long long
+test(char** name)
+{
+    *name = "moneytype_00 - obj_internal_FormatMoney";
+    
+    /** 0 Case **/
+    MoneyType test = {0};
+    assert(strcmp(objFormatMoneyTmp(&test, NULL), "$0.00") == 0);
+
+    /** Positive Case **/
+    test.Value = 70000;
+    assert(strcmp(objFormatMoneyTmp(&test, NULL), "$7.00") == 0);
+
+    /** Negative Case **/
+    test.Value = -70000;
+    assert(strcmp(objFormatMoneyTmp(&test, NULL), "-$7.00") == 0);
+
+    /** Nonzero Cent Case **/
+    test.Value = -75000;
+    assert(strcmp(objFormatMoneyTmp(&test, NULL), "-$7.50") == 0);
+
+    /** Fractional Cent Case **/
+    test.Value = -70001;
+    assert(strcmp(objFormatMoneyTmp(&test, NULL), "-$7.00") == 0);
+    
+    return 0;
+}
