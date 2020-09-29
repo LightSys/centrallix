9ef21dd1e00bfe287c2e01001a2f85823fd8d837
Carl Cederborg <45981643+cscederborg@users.noreply.github.com>
2020-09-28 23:38:44 -0600

    Working fp_internal_ParseColumn function and testing for 64bit MoneyType

--- centrallix/tests/test_moneytype_fp_internal_ParseColumn_00.c
+++ centrallix/tests/test_moneytype_fp_internal_ParseColumn_00.c
@@ -10,37 +10,41 @@ test(char** name)
 {
     *name = "moneytype_00 - fp_internal_ParseColumn";
     
-    //FpColInf testColInf = {0};
-    pFpColInf = &FpColInf;
+    //This Test is making the assumption that the raw data passed in testString here (row_data in objdrv_fp.c)
+    //is in units dollars.  So, the function now converts it to 1/10000ths like the new moneyType requires.
+    
+    FpColInf testColInf = {0};
+    pFpColInf pTestColInf = &testColInf;
     
     //7 for money type
-    FpColInf.Type = 7;
-    FpColInf.Length = 8;
-    FpColInf.RecordOffset = 0;
-    FpColInf.DecimalOffset = 4;
+    testColInf.Type = 7;
+    testColInf.Length = 8;
+    testColInf.RecordOffset = 0;
+    testColInf.DecimalOffset = 0;
     ObjData moneyData;
     pObjData moneyDataPtr = &moneyData;
     MoneyType testMoneyData = {0};
     pMoneyType pTestMoneyData = &testMoneyData;
-    
+
+    /** No Decimal Offset Case **/
     //Raw data for row data.
     char* testString = "450";
+    assert(fp_internal_ParseColumn(pTestColInf, moneyDataPtr, (char*)pTestMoneyData, testString) == 0);
     
-    assert(fp_internal_ParseColumn(pFpColInf, moneyDataPtr, (char*)pTestMoneyData, testString) == 0);
+    assert(moneyDataPtr->Money->Value == 4500000);
 
-    /** NULL Case **/
-
-    /** Positive Case **/
-    testExp->Types.Money.Value = 90000;
-    testExp->DataType = DATA_T_MONEY;
-    testExp->NodeType = EXPR_N_MONEY;
-
-
-    /** Negative Case **/
-    //test.Value = -70500;
-    //data_ptr = "Negative Seven And 05/100 ";
-    //returnStr = objDataToWords(7,&test);
-    //assert(strcmp(data_ptr, returnStr) == 0);
-
+    /** Decimal Offset, Value > 1 Case **/
+    //Value will be greater than one after dividing by
+    //10 a DecimalOffset # of times
+    testColInf.DecimalOffset = 1;
+    assert(fp_internal_ParseColumn(pTestColInf, moneyDataPtr, (char*)pTestMoneyData, testString) == 0);
+    assert(moneyDataPtr->Money->Value == 450000);
+    
+    /** Decimal Offset, Value < 1 Case **/
+    testColInf.DecimalOffset = 3;
+    assert(fp_internal_ParseColumn(pTestColInf, moneyDataPtr, (char*)pTestMoneyData, testString) == 0);
+    assert(moneyDataPtr->Money->Value == 4500);
+    
+    
     return 0;
 }
