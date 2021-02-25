1bb72bc0b883050e63bcc24ca256d3d89c6edc4e
Greg Beeley <gbeeley@users.sourceforge.net>
2021-02-25 15:25:34 -0700

    HTTP interface: handle EXEC statements in sql reopen

--- centrallix/netdrivers/net_http_osml.c
+++ centrallix/netdrivers/net_http_osml.c
@@ -469,6 +469,7 @@ nht_i_OSML(pNhtConn conn, pObject target_obj, char* request, pStruct req_inf, pN
     char* strval;
     XArray tail_buffer;
     int n_skipped;
+    int ws;
     
     handle_t session_handle;
     handle_t query_handle;
@@ -1118,12 +1119,15 @@ nht_i_OSML(pNhtConn conn, pObject target_obj, char* request, pStruct req_inf, pN
 				find_inf = stLookup_ne(nht_query->ParamData, "name");
 				if (find_inf)
 				    {
+				    /** Method #1: by parameter **/
 				    xsQPrintf(reopen_str, "string:V:%STR", ptr);
 				    stAddValue_ne(find_inf, xsString(reopen_str));
-				    xsQPrintf(reopen_str, "%STR FOR UPDATE", reopen_sql);
+				    ws = strspn(reopen_sql, " \t\r\n");
+				    xsQPrintf(reopen_str, "%STR%STR", reopen_sql, (strncasecmp(reopen_sql+ws, "exec", 4) == 0)?"":" FOR UPDATE");
 				    }
 				else
 				    {
+				    /** Method #2: by SQL clause **/
 				    xsQPrintf(reopen_str, "%STR %[WHERE%]%[HAVING%] :name = %STR&QUOT FOR UPDATE", reopen_sql, !reopen_having, reopen_having, ptr);
 				    }
 				qy = objMultiQuery(objsess, reopen_str->String, nht_query?nht_query->ParamList:NULL, 0);
