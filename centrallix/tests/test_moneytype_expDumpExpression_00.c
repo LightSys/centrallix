adca88df699c0f0da396e364522d91e903985a83
sheesania <sheesania@users.noreply.github.com>
2021-05-24 14:54:33 -0600

    Test for moneytype changes in exp_internal_DumpExpression_r

---
+++ centrallix/tests/test_moneytype_expDumpExpression_00.c
@@ -0,0 +1,29 @@
+#include "obj.h"
+#include <assert.h>
+#include <stdio.h>
+#include "expression.h"
+#include "cxlib/xstring.h"
+#include "testhelpers/stdoutHelper.h"
+
+long long
+test(char** name)
+{
+    *name = "moneytype_00 - expDumpExpression";
+    
+    pExpression testExp = expAllocExpression();
+    ObjData moneyData;
+    MoneyType money = {70000};
+    moneyData.Money = &money;
+    pObjData moneyDataPtr = &moneyData;
+
+    testExp = expPodToExpression(moneyDataPtr, 7, testExp);
+
+    stdoutHelper_startCapture();
+    expDumpExpression(testExp);
+    char* stdoutContents = stdoutHelper_stopCaptureAndGetContents();
+    assert(strstr(stdoutContents, "MONEY = 70000, 0 child(ren), money=$7.00, NEW"));
+
+    expFreeExpression(testExp);
+    
+    return 0;
+}
