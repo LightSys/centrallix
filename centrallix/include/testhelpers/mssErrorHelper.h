a5420a0b143526274c445ca294dbd66465964e70
sheesania <sheesania@users.noreply.github.com>
2021-05-24 14:39:07 -0600

    Better test helper doc strings

--- centrallix/include/testhelpers/mssErrorHelper.h
+++ centrallix/include/testhelpers/mssErrorHelper.h
@@ -1,8 +1,13 @@
 #ifndef _MSS_ERROR_HELPER_H
 #define _MSS_ERROR_HELPER_H
 
-/** doc string */
+/** Authenticates with Centrallix so errors are captured */
 int mssErrorHelper_init();
+
+/** 
+ * Returns 1 if the most recent call to mssError contains the string in "message", 0 otherwise. 
+ * You must call mssErrorHelper_init() first so calls to mssError are captured.
+ */
 int mssErrorHelper_mostRecentErrorContains(char *message);
 
 #endif /* not defined _MSS_ERROR_HELPER_H */
--- centrallix/include/testhelpers/stdoutHelper.h
+++ centrallix/include/testhelpers/stdoutHelper.h
@@ -4,7 +4,7 @@
 /** Begin redirecting and capturing stdout output. It will no longer output to the terminal (or wherever else you have it going) */
 int stdoutHelper_startCapture();
 
-/** Stop redirecting stdout and return the redirected contents that were captured */
+/** Stop redirecting stdout and return the redirected contents that were captured. You must call stdoutHelper_startCapture() first */
 char* stdoutHelper_stopCaptureAndGetContents();
 
 #endif /* not defined _STDOUT_HELPER_H */
