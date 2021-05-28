bae414d2c4e4d37ee3b92456a0308092633df09d
Zachary Allen <zacharys.allen@calbaptist.edu>
2021-05-28 14:51:07 -0600

    Added some more tests for more fringe cases/regular use cases

--- centrallix-lib/tests/test_b+tree3_11.c
+++ centrallix-lib/tests/test_b+tree3_11.c
@@ -9,19 +9,33 @@
 long long
 test(char** tname)
    	{
-
+    
     int y;
+
+
+    *tname = "b+tree3_11 Test bptDeInit Function at 2 levels";
+    pBPTree tree = bptNew();
 
     
 
-	*tname = "b+tree3_10 Test bptDeInit function on an empty tree";
-    pBPTree tree = bptNew();
+    int *info1 = nmMalloc(sizeof(int));
+    *info1 = 10;
+
+    int *info2 = nmMalloc(sizeof(int));
+    *info1 = 20;
+
+    int *info3 = nmMalloc(sizeof(int));
+    *info1 = 30;
+
+    bptAdd(tree, "0001", 4, info1);
+    bptAdd(tree, "0002", 4, info2);
+    bptAdd(tree, "0003", 4, info3);
 
     y = bptDeInit(tree);
 
     assert(y == 0);
 
-
+    
     //This next part is just to avoid a floating point error
     int i;
     int x;
@@ -31,5 +45,7 @@ test(char** tname)
     }
 
 
+
     return 10;
    	}
+
---
+++ centrallix-lib/tests/test_b+tree3_12.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include "b+tree.h"
+#include "newmalloc.h"
+
+// dummy free function
+int bpt_dummy_freeFn(void* arg, void* ptr) {
+    free(ptr);
+    return 0;
+}
+
+long long
+test(char** tname)
+   	{
+	*tname = "b+tree3_12 Removing then Adding what you Removed";
+	
+   int y;
+   pBPTree this = bptNew();
+
+   int *info1 = malloc(sizeof(int));
+   int *info2 = malloc(sizeof(int));
+   int *info3 = malloc(sizeof(int));
+
+   *info1 = 10;
+   *info2 = 20;
+   *info3 = 30;
+
+    bptAdd(this, "Key1", 4, info1);
+    bptAdd(this, "Key2", 4, info2);
+    bptAdd(this, "Key3", 4, info3);
+    
+    y = bptRemove(this, "Key1", 4, bpt_dummy_freeFn, NULL);
+    assert(y == 0);
+
+    y = bptAdd(this, "Key1", 4, info1);
+    assert(y == 0);
+
+
+
+
+
+
+    //This next part is just to avoid a floating point error
+    int i;
+    int x;
+    x = 1;
+    for (i = 0; i < 10000000; i++) {
+        x++;
+    }
+
+    return 10;
+   	}
+
+
+
