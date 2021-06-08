b5d7a011aabf2d4b362d3e10bd73406c8de2d613
Zachary Allen <zacharys.allen@calbaptist.edu>
2021-06-08 11:22:16 -0600

    tests 16 and 17 to BREAK iterator

---
+++ centrallix-lib/tests/test_b+tree_17.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include "b+tree.h"
+#include "newmalloc.h"
+
+int free_func(void* args, void* ref){
+    nmFree(ref, sizeof(int));
+    return 0;
+}
+
+long long
+test(char** tname)
+   	{
+    
+    *tname = "b+tree_16 Iterator/bptDeInit kinda breaks when used in conjunction";
+    pBPTree tree = bptNew();
+    int status = 0;
+    int ret;
+
+    
+
+    int *info1 = nmMalloc(sizeof(int));
+    *info1 = 10;
+
+    int *info2 = nmMalloc(sizeof(int));
+    *info2 = 20;
+
+    int *info3 = nmMalloc(sizeof(int));
+    *info3 = 30;
+
+    int *info4 = nmMalloc(sizeof(int));
+    *info4 = 40;
+
+    int *info5 = nmMalloc(sizeof(int));
+    *info5 = 50;
+
+    int *info6 = nmMalloc(sizeof(int));
+    *info6 = 60;
+
+    bptAdd(tree, "0001", 4, info1);
+    bptAdd(tree, "0002", 4, info2);
+    bptAdd(tree, "0003", 4, info3);
+    bptAdd(tree, "0004", 4, info4);
+    bptAdd(tree, "0005", 4, info5);
+    bptAdd(tree, "0006", 4, info6);
+
+    pBPIter iterator = bptFront(tree);
+
+    ret = bptFree(tree, free_func, NULL);
+    assert(ret == 0);
+
+    printf("test1\n");
+    //printf("%s\n", iterator->Curr->Keys[iterator->Index].Value);
+    printf("test2\n");
+
+    bptNext(iterator, &status);
+
+    /*
+    It seems like this is breaking because, while it has been technically deinitialized, the computer has yet to override the data stored there
+    , and so it is the same data stored in the memory. See test 17 for bptFree testing
+    */
+
+    printf("%d\n", status);
+    printf("%s\n", iterator->Curr->Keys[iterator->Index].Value);
+
+    assert(status == 0);
+
+    
+    //This next part is just to avoid a floating point error
+    int i;
+    int x;
+    x = 1;
+    for (i = 0; i < 10000000; i++) {
+        x++;
+    }
+
+
+
+    return 10;
+   	}
+
