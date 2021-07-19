a7b6b80c3d5459b56b42d73d2c13a3f96dd643a1
sheesania <sheesania@users.noreply.github.com>
2021-07-19 14:25:48 -0600

    Test for expDumpExpression with money > INT_MAX

--- centrallix/tests/test_moneytype_expDumpExpression_00.c
+++ centrallix/tests/test_moneytype_expDumpExpression_00.c
@@ -16,12 +16,20 @@ test(char** name)
     moneyData.Money = &money;
     pObjData moneyDataPtr = &moneyData;
 
-    testExp = expPodToExpression(moneyDataPtr, 7, testExp);
+    testExp = expPodToExpression(moneyDataPtr, DATA_T_MONEY, testExp);
 
     stdoutHelper_startCapture();
     expDumpExpression(testExp);
     char* stdoutContents = stdoutHelper_stopCaptureAndGetContents();
     assert(strstr(stdoutContents, "MONEY = 70000, 0 child(ren), money=$7.00, NEW"));
+
+    /** Money > INT_MAX **/
+    money.Value = 18888888888888888;
+    testExp = expPodToExpression(moneyDataPtr, DATA_T_MONEY, testExp);
+    stdoutHelper_startCapture();
+    expDumpExpression(testExp);
+    stdoutContents = stdoutHelper_stopCaptureAndGetContents();
+    assert(strstr(stdoutContents, "MONEY = 18888888888888888, 0 child(ren), money=$1888888888888.88, NEW"));
 
     expFreeExpression(testExp);
     
