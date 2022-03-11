36c6aeb3f3d68c3a08a23079450b7cb55df504cf
John Delano <jdelano@cedarville.edu>
2022-03-10 20:36:44 -0700

    Cleaned up similarity test cases.

--- centrallix/tests/test_similarity_02.c
+++ centrallix/tests/test_similarity_02.c
@@ -6,17 +6,6 @@ long long
 test(char** name)
     {
     *name = "similarity_02 ** SHOULD PASS **";
-    // pXHashTable table = nmSysMalloc(sizeof(XHashTable));    
-    // typedef struct d_data_node { int key; double value; } freq_node;
-    
-    // exp_fn_i_frequency_table(table, "Hello");
-    // exp_fn_i_relative_frequency_table(table);
-
-    // double dot_product;
-    // exp_fn_i_dot_product(&dot_product, table, table);
-    // assert(dot_product == 0.28);
-    // xhDeInit(table);
-    // nmSysFree(table);
 
     double *table = nmMalloc(36 * sizeof(double));
     exp_fn_i_frequency_table(table, "Hello");
--- centrallix/tests/test_similarity_04.c
+++ centrallix/tests/test_similarity_04.c
@@ -6,17 +6,6 @@ long long
 test(char** name)
     {
     *name = "similarity_04 ** SHOULD PASS **";
-    // pXHashTable table = nmSysMalloc(sizeof(XHashTable));    
-    // typedef struct d_data_node { int key; double value; } freq_node;
-    
-    // exp_fn_i_frequency_table(table, "Hello");
-    // exp_fn_i_relative_frequency_table(table);
-
-    // double magnitude;
-    // exp_fn_i_magnitude(&magnitude, table, table);
-    // assert(magnitude >= 0.529150 && magnitude <= 0.529151);
-    // xhDeInit(table);
-    // nmSysFree(table);
 
     double *table = nmMalloc(36 * sizeof(double));
     exp_fn_i_frequency_table(table, "Hello");
