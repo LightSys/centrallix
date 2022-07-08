4c6d91f8a37365f51a7ca797d4cf522acdfb29ab
Greg Beeley <Greg.Beeley@LightSys.org>
2022-07-08 10:39:23 -0600

    JS: apply send_updates logic to osrc deletes as well as updates.

--- centrallix-os/sys/js/htdrv_osrc.js
+++ centrallix-os/sys/js/htdrv_osrc.js
@@ -775,9 +775,15 @@ function osrc_action_delete(aparam) //up,initiating_client)
 
     //Delete an object through OSML
     //var src = this.baseobj + '?cx__akey='+akey+'&ls__mode=osml&ls__req=delete&ls__sid=' + this.sid + '&ls__oid=' + up.oid;
-    this.initiating_client = initiating_client;
-    this.deleteddata=up;
-    this.DoRequest('delete', this.baseobj, {ls__oid:up.oid}, osrc_action_delete_cb);
+    if (this.send_updates)
+	{
+	this.initiating_client = initiating_client;
+	this.DoRequest('delete', this.baseobj, {ls__oid:up.oid}, osrc_action_delete_cb);
+	}
+    else
+	{
+	this.DeleteRecord(this.CurrentRecord, initiating_client);
+	}
     //this.initiating_client.ObjectDeleted();
     //this.initiating_client.OperationComplete();
     return 0;
@@ -789,51 +795,56 @@ function osrc_action_delete_cb()
     var initiating_client = this.initiating_client;
     if(links && links[0] && links[0].target != 'ERR')
 	{
-	var recnum=this.CurrentRecord;
-	var cr=this.replica[recnum];
-	if(cr)
-	    {
-	    // Remove the deleted row
-	    delete this.replica[recnum];
-
-	    // Adjust replica row id's to fill up the 'hole'
-	    for(var i=recnum; i<this.LastRecord; i++)
-		{
-		this.replica[i] = this.replica[i+1];
-		this.replica[i].id = i;
-		}
-	    delete this.replica[this.LastRecord];
-	    if (this.FinalRecord == this.LastRecord)
-		this.FinalRecord--;
-	    this.LastRecord--;
-	    if (this.OSMLRecord > 0) this.OSMLRecord--;
-
-	    // Notify osrc clients (forms/tables/etc)
-	    for(var i in this.child)
-		this.child[i].ObjectDeleted(recnum, this);
-
-	    // Need to fetch another record (delete was on last one in replica)?
-	    this.SyncID = osrc_syncid++; // force any client osrc's to resync
-	    if (this.CurrentRecord > this.LastRecord)
-		{
-		this.CurrentRecord--;
-		this.MoveToRecord(this.CurrentRecord+1, true);
-		}
-	    else
-		{
-		this.MoveToRecord(this.CurrentRecord, true);
-		}
-	    }
-	if (initiating_client) initiating_client.OperationComplete(true, this);
+	this.DeleteRecord(this.CurrentRecord, initiating_client);
 	}
     else
 	{
 	// delete failed
-	if (initiating_client) initiating_client.OperationComplete(false, this);
+	if (initiating_client)
+	    initiating_client.OperationComplete(false, this);
 	}
     this.initiating_client=null;
-    delete this.deleteddata;
     return 0;
+    }
+
+function osrc_delete_record(recnum, client)
+    {
+    var cr=this.replica[recnum];
+    if(cr)
+	{
+	// Remove the deleted row
+	delete this.replica[recnum];
+
+	// Adjust replica row id's to fill up the 'hole'
+	for(var i=recnum; i<this.LastRecord; i++)
+	    {
+	    this.replica[i] = this.replica[i+1];
+	    this.replica[i].id = i;
+	    }
+	delete this.replica[this.LastRecord];
+	if (this.FinalRecord == this.LastRecord)
+	    this.FinalRecord--;
+	this.LastRecord--;
+	if (this.OSMLRecord > 0) this.OSMLRecord--;
+
+	// Notify osrc clients (forms/tables/etc)
+	for(var i in this.child)
+	    this.child[i].ObjectDeleted(recnum, this);
+
+	// Need to fetch another record (delete was on last one in replica)?
+	this.SyncID = osrc_syncid++; // force any client osrc's to resync
+	if (this.CurrentRecord > this.LastRecord)
+	    {
+	    this.CurrentRecord--;
+	    this.MoveToRecord(this.CurrentRecord+1, true);
+	    }
+	else
+	    {
+	    this.MoveToRecord(this.CurrentRecord, true);
+	    }
+	}
+    if (client)
+	client.OperationComplete(true, this);
     }
 
 function osrc_action_create(aparam)
@@ -4050,6 +4061,7 @@ function osrc_init(param)
     loader.MoveToRecord=osrc_move_to_record;
     loader.MoveToRecordCB=osrc_move_to_record_cb;
     loader.Clear = osrc_action_clear;
+    loader.DeleteRecord = osrc_delete_record;
     loader.child =  [];
     loader.oldoids =  [];
     loader.sid = null;
