7cc0b3b0dc1c38a81c8f8079959933549a6da095
bwutee <leeminsik999@gmail.com>
2025-06-04 17:15:13 -0600

    Add menu test

---
+++ centrallix-ui-test/tests/menu_test.py
@@ -0,0 +1,204 @@
+# Minsik Lee May 2025
+# NOTE: USE ChromeDriverManager. Pip install it.
+
+"""Notes"""
+
+""" Module allowing web testing using pure Selenium """
+
+import toml
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def create_driver(test_url) -> webdriver.Chrome:
+    """Create and return a configured Chrome WebDriver."""
+    service = Service(ChromeDriverManager().install())
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--lang=en')
+    chrome_options.add_argument('--incognito')
+    # Skip Certificatgite/by pass it
+    chrome_options.add_argument('--ignore-certificate-errors')
+
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.set_window_size(1920, 1080)
+    driver.get(test_url)
+
+    # Wait until the page has fully loaded
+    WebDriverWait(driver, 10).until(
+        lambda d: d.execute_script("return document.readyState") == "complete"
+    )
+
+    return driver
+
+
+def get_label_info(driver: webdriver.Chrome):
+    """Return status of the label."""
+    try:
+        label_element = driver.find_element(
+            By.XPATH, "//div[contains(@id, 'lbl')]//span").text
+        return (label_element)
+    except Exception as e:
+        print(f"Error retrieving label properties: {e}")
+
+
+def run_test():
+    """Run menu test."""
+    try:
+        config = toml.load("config.toml")
+    except FileNotFoundError:
+        print("Config.toml is missing. Make sure to rename config.template and try again.")
+        return
+
+    test_url = config["url"] + "/tests/ui/menu_test.app"
+    driver = create_driver(test_url)
+    action = ActionChains(driver)
+
+    try:
+        # Wait for menu to appear
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, "//div[contains(@id, 'mn')]"))
+        )
+
+        # Get main menu element
+        prefix = "mn"
+        suffix = "main"
+        xpath_expression = (
+            f"//div[starts-with(@id, '{prefix}') and "
+            f"substring(@id, string-length(@id) - string-length('{suffix}') + 1) = '{suffix}' and "
+            f"number(substring(@id, string-length('{prefix}') + 1, string-length(@id) - string-length('{prefix}') - string-length('{suffix}'))) = "
+            f"number(substring(@id, string-length('{prefix}') + 1, string-length(@id) - string-length('{prefix}') - string-length('{suffix}')))]"
+        )
+        main_menu_elems = driver.find_elements(By.XPATH, xpath_expression)
+
+        # TEST1: Mouse Over
+        # - Test mouse over functionality. For menu expand to display menu items
+        print("TEST1: Mouse over")
+
+        # get "mn##high" div that changes its syle when mouse hover
+        high_div = main_menu_elems[0].find_element(
+            By.XPATH, "./div[contains(@id, 'high')]")
+        init_high_style = 0
+
+        menu_items_elems = main_menu_elems[0].find_elements(
+            By.XPATH, ".//td[(@nowrap and @valign = 'middle')]")
+
+        # Hover mouse over menu items
+        for i in range(len(menu_items_elems)):
+            action.move_to_element(menu_items_elems[i]).pause(1.5).perform()
+
+            high_style = int(
+                ''.join(filter(str.isdigit, high_div.get_attribute("style").split(";")[3])))
+            if high_style < init_high_style:
+                raise Exception("[TEST1] hover failed")
+
+            updated_main_menu_elems = driver.find_elements(
+                By.XPATH, xpath_expression)
+            if updated_main_menu_elems != main_menu_elems:
+                print(f"\tItem {i+1} Menu Expanded")
+
+            init_high_style = high_style
+            main_menu_elems = updated_main_menu_elems
+
+        print(f"TEST1 PASS - mouse over on {len(menu_items_elems)} items.\n")
+
+        # TEST2: Click a Menu Item
+        print("TEST2: Click Menu Items")
+        # - Test if menu items close after click
+        # - Test if select event is triggered properly
+
+        # Child window element XPATH
+        prefix = "wn"
+        suffix = "base"
+        xpath_expression = (
+            f"//div[starts-with(@id, '{prefix}') and "
+            f"substring(@id, string-length(@id) - string-length('{suffix}') + 1) = '{suffix}' and "
+            f"number(substring(@id, string-length('{prefix}') + 1, string-length(@id) - string-length('{prefix}') - string-length('{suffix}'))) = "
+            f"number(substring(@id, string-length('{prefix}') + 1, string-length(@id) - string-length('{prefix}') - string-length('{suffix}')))]"
+        )
+
+        # Iterate through main menu elements
+        for idx in range(len(menu_items_elems)):
+            effectFlag = False
+            action.click(menu_items_elems[idx]).perform()
+            item_name = menu_items_elems[idx].text
+            print(f"\tclicked {item_name}")
+            
+            # Test sub menu items
+            sub_menu_idx = -1
+
+            for i in range(len(main_menu_elems)):
+              if "visibility: inherit" in main_menu_elems[i].get_attribute("style"):
+                  sub_menu_idx = i
+                  break
+
+            if sub_menu_idx != -1:
+                sub_menu_items = main_menu_elems[sub_menu_idx].find_elements(
+                    By.XPATH, ".//td[(@nowrap and @valign = 'middle')]")
+
+                print(f"\ttesting '{item_name}' menu ...")
+                for item in sub_menu_items:
+                    flag = False
+                    item_name = item.text
+                    label_before = get_label_info(driver)
+
+                    action.click(item).perform()
+                    print(f"\t\tclicked {item_name}")
+                    time.sleep(1)
+
+                    # If opened a child window
+                    child_window_elem = driver.find_element(
+                        By.XPATH, xpath_expression)
+                    child_window_state = child_window_elem.get_attribute(
+                        "style")
+                    if "visibility: inherit" in child_window_state:
+                        print(
+                            f"\t\tClicking '{item_name}' opend child window.")
+                        flag = True
+                        action.click(child_window_elem.find_element(
+                            By.XPATH, ".//img[contains(@name, 'close')]")).perform()
+
+                    # If triggered select event
+                    if label_before != get_label_info(driver):
+                        flag = True
+
+                    if flag == False:
+                        raise Exception(
+                            f"Clicking '{item_name}' had no effect")
+
+                    action.click(menu_items_elems[idx]).perform()
+            else:
+                #If triggered select event
+                if label_before != get_label_info(driver):
+                    effectFlag = True
+                    
+                if effectFlag == False:
+                    raise Exception(f"clicking '{item_name}' had no effect")
+        print("TEST2 PASS - Item click properly responded\n")
+        
+        # TEST3: Right Click Popup
+        print("TEST3: Right Click Popup")
+        
+        label_element = driver.find_element(
+            By.XPATH, "//div[contains(@id, 'lbl')]//span")
+        action.move_to_element_with_offset(label_element, 30, 30).context_click().perform()
+        for i in range(len(main_menu_elems)):
+              if "visibility: inherit" in main_menu_elems[i].get_attribute("style"):
+                  print("\tTEST3 PASS - Right click opened popup")
+                  
+        print("Test All Passed\nExiting ...")
+
+    finally:
+        # Cleanup
+        time.sleep(10)
+        driver.quit()
+
+
+if __name__ == "__main__":
+    run_test()
