fc7f9af3106e7cfe1c1f7de1745a98e8da9136d0
Lightning11wins <lightning11wins+github@gmail.com>
2026-02-06 17:23:47 -0700

    Add a warning when using the pane resize action, which breaks responsiveness.

--- centrallix-os/sys/js/htdrv_pane.js
+++ centrallix-os/sys/js/htdrv_pane.js
@@ -79,12 +79,9 @@ function pn_setbackground(aparam)
 
 function pn_action_resize(aparam)
     {
-    // I think this code is a better implementation of the following lines:
-    // var w = (aparam.Width)  ?? pg_get_style(this, 'width');
-    // var h = (aparam.Height) ?? pg_get_style(this, 'height');
-
-    var w = aparam.Width?aparam.Width:pg_get_style(this, 'width');
-    var h = aparam.Height?aparam.Height:pg_get_style(this, 'height');
+    console.warn('Resize action called which will probably break responsiveness.', this, aparam);
+    const w = (aparam.Width)  ?? pg_get_style(this, 'width');
+    const h = (aparam.Height) ?? pg_get_style(this, 'height');
     resizeTo(this, w, h);
     }
 
