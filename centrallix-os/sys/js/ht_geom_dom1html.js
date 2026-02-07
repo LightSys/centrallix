a74257653f1b983b3e59deb1c2d9bf0684111b35
Lightning11wins <lightning11wins+github@gmail.com>
2026-02-06 17:18:38 -0700

    Add tools to ht_geom_dom1html.
    Add Math.clamp() and Math.isBetween().
    Add getParentSize(), getParentW(), and getParentH().
    Refactor getRelativeX/Y/W/H() to call the new getRelative().
    Refactor setRelativeX/Y/W/H() to call the new setRelative().
    Add fast_setRelativeX/Y().
    Add setResponsiveX/Y/W/H() using a new shared setResponsive().
    Add responsiveness to moveTo() with the new functions.
    
    Fix style guide mistakes.
    Clean up.

--- centrallix-os/sys/js/ht_geom_dom1html.js
+++ centrallix-os/sys/js/ht_geom_dom1html.js
@@ -11,34 +11,61 @@
 
 // Cross browser Geometry DOM1HTML
 
+// Add some useful functions to Math that will be needed elsewhere.
+// Math.clamp = (min, val, max) => Math.min(Math.max(min, val), max);
+// Math.isBetween = (lowerBound, num, upperBound) => lowerBound < num && num < upperBound;
+
+// Dev debug versions.
+Math.clamp = (min, val, max) =>
+    {
+    if (min > max || isNaN(min) || isNaN(val) || isNaN(max))
+	{
+	console.warn(`Math.clamp(${min}, ${val}, ${max});`);
+	console.trace();
+	}
+    return Math.min(Math.max(min, val), max);
+    }
+Math.isBetween = (lowerBound, num, upperBound) =>
+    {
+    if (lowerBound > upperBound || isNaN(lowerBound) || isNaN(num) || isNaN(upperBound))
+        console.warn(`Math.isBetween(${lowerBound}, ${num}, ${upperBound});`);
+    return lowerBound < num && num < upperBound;
+    }
+
+
+
 /*** Experimental system for turning off clipping CSS.
  *** The clip values are still stored and can be queried
  *** for legacy compatibility, but they will not output
  *** any clip rectangles or clip paths in the CSS or HTML.
  ***/
 /** Ensure clipping is disabled for a layer / HTML node. **/
-function disableClippingCSS(l) {
+function disableClippingCSS(l)
+    {
     console.log(`Turning off clipping for ${l.clip.obj.id}.`);
     l.clip.noclip = true;
     updateClippingCSS(l);
-}
+    }
 /** Ensure clipping is enabled for a layer / HTML node. **/
-function enableClippingCSS(l) {
+function enableClippingCSS(l)
+    {
     l.clip.noclip = false;
     updateClippingCSS(l);
-}
+    }
 /** Update clipping without changing any specific values. **/
-function updateClippingCSS(l) {
+function updateClippingCSS(l)
+    {
     setClipTop(l, getClipTop(l));
-}
+    }
 
 /** Debug function for finding clipped dom nodes. **/
-function getClipped() {
+function getClipped()
+    {
     return Array
-    	.from(Window.clipped)
-    	.filter(id=>id)
+	.from(Window.clipped)
+	.filter(id=>id)
 	.map(id=>document.getElementById(id));
-}
+    }
 
 // Clip Width
 function getClipWidth(l) 
@@ -236,43 +263,192 @@ function getpageYOffset()
     return window.pageYOffset;
     }
 
-function getRelativeX(l)
+/*** Get the size of a DOM node's parent container.
+ *** 
+ *** @param l The DOM node.
+ *** @returns The width and height of the parent container.
+ ***/
+function getParentSize(l)
     {
-    if (l.__pg_left != null) return l.__pg_left;
-    var left = parseInt(pg_get_style(l,'left'));
-    l.__pg_left = isNaN(left)?0:left;
-    return l.__pg_left;
+    const parentRect = l.parentNode.getBoundingClientRect();
+    return { width: parentRect.width, height: parentRect.height };
     }
 
-function setRelativeX(l, value)
+/*** Get the width of a DOM node's parent container.
+ *** 
+ *** @param l The DOM node.
+ *** @returns The width of the parent container.
+ ***/
+function getParentW(l)
     {
-    pg_set_style(l,'left',(l.__pg_left = parseInt(value)));
-    return l.__pg_left;
+    return getParentSize(l).width;
     }
 
-function getRelativeY(l)
+/*** Get the height of a DOM node's parent container.
+ *** 
+ *** @param l The DOM node.
+ *** @returns The height of the parent container.
+ ***/
+function getParentH(l)
     {
-    if (l.__pg_top != null) return l.__pg_top;
-    return (l.__pg_top = parseInt(pg_get_style(l,'top')));
+    return getParentSize(l).height;
     }
 
-function setRelativeY(l, value)
+
+/*** Problem:
+ *** If the programmer calls setRelativeX() (or a similar function, such as moveTo() or moveBy()),
+ *** they might be using a value they got from the server, based on the resolution when the page
+ *** was first loaded. However, they might also be using a value they got dynamically by calling
+ *** some function to check the actual size of an element. Previously, this distinction did not
+ *** matter because these values would be the same. However, now that pages can be resized on the
+ *** client, it does matter.
+ ***/
+
+ 
+/*** We ignore the current value of __pg_left in the following functions even
+ *** though it might be correct and faster than querying the DOM. However, the
+ *** layout may have changed since last time, so we always requery the DOM.
+ ***/
+function getRelative(l, d)
     {
-    pg_set_style(l,'top',(l.__pg_top = parseInt(value)));
-    return l.__pg_top;
+    const val = parseInt(pg_get_style(l, d, NaN));
+    return l['__pg_' + d] = (isNaN(val)) ? 0 : val;
     }
 
+function getRelativeX(l) { return getRelative(l, 'left'); }
+function getRelativeY(l) { return getRelative(l, 'top'); }
+function getRelativeW(l) { return getRelative(l, 'width'); }
+function getRelativeH(l) { return getRelative(l, 'height'); }
+
+/*** Sets the location of a DOM node relative to its parent container.
+ *** 
+ *** @param l The DOM node being set. (Assumed to be defined.)
+ *** @param value The new location. This can be a CSS string.
+ *** @param {'left'|'top'|'width'|'height'} d The dimension being set.
+ ***/
+function setRelative(l, value, d)
+    {
+    /** Convert the value to a number, if possible. **/
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) value = parsedValue;
+
+    pg_set_style(l, d, value);
+    l['__pg_' + d + '_style'] = value;
+    return l['__pg_' + d] = parseInt(pg_get_style(l, d));
+    }
+
+function setRelativeX(l, value) { return setRelative(l, value, 'left'); }
+function setRelativeY(l, value) { return setRelative(l, value, 'top'); }
+function setRelativeW(l, value) { return setRelative(l, value, 'width'); }
+function setRelativeH(l, value) { return setRelative(l, value, 'height'); }
+
+/*** Use these functions only in situations where performances is required
+ *** and you (a) value does not need to be parsed and (b) you do not need
+ *** the return value of the function.
+ ***/
+function fast_setRelativeX(l, value)
+    {
+    pg_set_style(l, 'left', value);
+    l['__pg_x_style'] = value;
+    }
+function fast_setRelativeY(l, value)
+    {
+    pg_set_style(l, 'top', value);
+    l['__pg_y_style'] = value;
+    }
+
+/*** Sets a dimension of a DOM element using coordinates in the server
+ *** generated adaptive layout. It is RECOMMENDED to call a specific sub-
+ *** function (aka. setResponsiveX(), setResponsiveY(), etc.) instead of
+ *** calling this function directly to avoid passing dimension directly.
+ ***
+ *** WARNING: Ensure that any value passed is calculated ENTIRELY using
+ *** values from the server (e.g. widget properties) and no values from
+ *** real page dimensions are used, as these change when the page is
+ *** resized after being loaded for the first time.
+ ***
+ *** @param l The DOM node being set. (Assumed to be defined.)
+ *** @param value The new location in server-side px. This value must be
+ *** 		  parseable as a number.
+ *** @param {'x'|'y'|'w'|'h'} d The letter for the dimension being set.
+ ***/
+function setResponsive(l, value, d) {
+    /** Convert the value to a number, if possible. **/
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) value = parsedValue;
+    
+    /** Server-layout values are always numbers. **/
+    if (typeof(value) !== 'number')
+	{
+	console.warn(`setResponsive(${l.id}, ?, '${d}'): Expected value to be a parseable number but got:`, value);
+	return value;
+	}
+    
+    /** The flexibility specified by the server. **/
+    var fl_scale = l['__fl_scale_' + d] ?? wgtrGetServerProperty(l, 'fl_scale_' + d);
+    if (fl_scale == undefined || fl_scale == null)
+	{
+	/** The server did not specify a flexibility, even though one was expected. **/
+	const warningMsg = 'setResponsive() - FAIL: Missing ' + ((wgtrIsNode(l)) ? 'wgtr.' : '__') + 'fl_scale_' + d;
+	console.warn(warningMsg, l);
+	fl_scale = 0;
+	}
+    
+    /** Inflexible elements don't need to be responsive. **/
+    if (fl_scale <= 0) return setRelative(l, value, d);
+    
+    /** The parent width expected by the server in the adaptive layout. **/
+    var d2 = d;
+    if (d2 == 'x') d2 = 'w';
+    if (d2 == 'y') d2 = 'h';
+
+    var fl_parent = l['__fl_parent_' + d2] ?? wgtrGetServerProperty(l, 'fl_parent_' + d2);
+    if (fl_parent == undefined || fl_parent == null)
+	{
+	const warningMsg = 'setResponsive() - FAIL: Missing ' + ((wgtrIsNode(l)) ? 'wgtr.' : '__') + 'fl_parent_' + d2;
+	console.warn(warningMsg, l);
+	}
+
+    /** Generate and set the CSS. **/
+    const css = `calc(${value}px + (100% - ${fl_parent}px) * ${fl_scale})`;
+    const prop = { x:'left', y:'top', w:'width', h:'height' }[d];
+    return setRelative(l, css, prop);
+}
+
+/** Call these functions instead of calling setResponsive() directly, which leads to less readable code. **/
+function setResponsiveX(l, value) { return setResponsive(l, value, 'x'); }
+function setResponsiveY(l, value) { return setResponsive(l, value, 'y'); }
+function setResponsiveW(l, value) { return setResponsive(l, value, 'w'); }
+function setResponsiveH(l, value) { return setResponsive(l, value, 'h'); }
+
+/** Moves a DOM node to a location within the window. **/
 function moveToAbsolute(l, x, y)
     {
     setPageX(l,x);
     setPageY(l,y);
     }
 
-function moveTo(l, x, y)
+/*** Moves a DOM node to a location inside it's parent container.
+ *** 
+ *** @param l The DOM node being moved.
+ *** @param x The new x coordinate. Can be a CSS string (if responsive is false).
+ *** @param y The new y coordinate. Can be a CSS string (if responsive is false).
+ *** @param responsive Whether the given coordinates should be treated as
+ *** 		       adaptive, 'server-side', coordinates where setResponsive()
+ *** 		       should be invoked to give them responsive design.
+ ***/
+function moveTo(l, x, y, responsive = false)
     {
-    //pg_set_style_string(this,'position','absolute');
-    setRelativeX(l,x);
-    setRelativeY(l,y);
+    if (responsive)
+	{
+	setResponsiveX(l, x);
+	setResponsiveY(l, x);
+	}
+    else
+	{
+	setRelativeX(l, x);
+	setRelativeY(l, y);
+	}
     }
 
 
