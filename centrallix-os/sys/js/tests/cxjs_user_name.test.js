6d234b24149d6431e2418fa106767c2bf3706c4d
Lightning11wins <lightning11wins+github@gmail.com>
2026-06-16 14:34:46 -0600

    Add testing for cxjs_user_name as a proof-of-concept.

---
+++ centrallix-os/sys/js/tests/cxjs_user_name.test.js
@@ -0,0 +1,34 @@
+// Copyright (C) 2026 LightSys Technology Services, Inc.
+//
+// You may use these files and this library under the terms of the
+// GNU Lesser General Public License, Version 2.1, contained in the
+// included file "COPYING" or http://www.gnu.org/licenses/lgpl.txt.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+'use strict';
+const { describe, it } = require('node:test');
+const assert           = require('node:assert/strict');
+const env              = require('./_setup');
+
+describe('cxjs_user_name', () =>
+    {
+    const cases =
+        [
+        'alice',
+        'bob',
+        ' !@#$%^&*()":;\' ',
+        ];
+
+    for (const name of cases)
+        {
+        it(`returns pg_username (\"${name}\")`, () =>
+            {
+            env.pg_username = name;
+            assert.equal(env.cxjs_user_name(), name);
+            });
+        }
+    });
