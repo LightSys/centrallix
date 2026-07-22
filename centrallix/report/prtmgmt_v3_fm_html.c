1571dea1fc0d5e613ba10f215a0dce1640e438f8
Lightning11wins <lightning11wins+github@gmail.com>
2026-07-22 12:22:14 -0600

    Minify HTML by removing explicit spacing.

--- centrallix/report/prtmgmt_v3_fm_html.c
+++ centrallix/report/prtmgmt_v3_fm_html.c
@@ -120,18 +120,18 @@
     "<!DOCTYPE html>\n" \
     "<html lang=\"en\">\n" \
     "<head>\n" \
-    "    <title>Centrallix HTML Document</title>\n" \
-    "    <meta charset=\"utf-8\">\n" \
-    "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n" \
-    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" \
-    "    <meta name=\"pragma\" content=\"no-cache\">\n" \
-    "    <meta name=\"referrer\" content=\"same-origin\">\n" \
-    "    <meta name=\"generator\" content=\"Centrallix PRTMGMT v3.0\">\n" \
-    /** Universal email styles. **/ \
-    "    <style>\n" \
-    "        body { font-family: \"Courier New\",Courier,fixed; }\n" \
-    "        td { vertical-align: top; line-height: 1; }\n" \
-    "    </style>\n" \
+	"<title>Centrallix HTML Document</title>\n" \
+	"<meta charset=\"utf-8\">\n" \
+	"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n" \
+	"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" \
+	"<meta name=\"pragma\" content=\"no-cache\">\n" \
+	"<meta name=\"referrer\" content=\"same-origin\">\n" \
+	"<meta name=\"generator\" content=\"Centrallix PRTMGMT v3.0\">\n" \
+	/** Universal email styles. **/ \
+	"<style>\n" \
+	    "body { font-family: \"Courier New\",Courier,fixed; }\n" \
+	    "td { vertical-align: top; line-height: 1; }\n" \
+	"</style>\n" \
     "</head>\n" \
     "<body style=\"background-color: %s;\">\n"
 
@@ -146,30 +146,26 @@
  ***    (1) %d	Width of table, pixels
  ***/
 #define PRT_HTMLFM_PAGEHEADER \
-    "    <center>\n" \
-    "    <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#606060\">\n" \
-    "        <tr>\n" \
-    "            <td bgcolor=\"#000000\">\n" \
-    "            <table width=\"%d\" border=\"0\" cellspacing=\"1\" cellpadding=\"16\">\n" \
-    "                <tr><td width=\"100%\" bgcolor=\"#ffffff\">\n" \
-    "<!------------------------------PAGE BEGIN------------------------------>\n" \
-    "\n"
+	"<center>\n" \
+	"<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#606060\">\n" \
+	    "<tr>\n" \
+		"<td bgcolor=\"#000000\">\n" \
+		"<table width=\"%d\" border=\"0\" cellspacing=\"1\" cellpadding=\"16\">\n" \
+		    "<tr><td width=\"100%\" bgcolor=\"#ffffff\">\n\n"
 
 
 /*** Page footer - end the page ***/
-#define PRT_HTMLFM_PAGEFOOTER \
-    "\n" \
-    "<!------------------------------PAGE END-------------------------------->\n" \
-    "                </td></tr>\n" \
-    "            </table>\n" \
-    "            </td><td valign=\"top\" align=\"left\" width=\"8\"><table width=\"8\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#c0c0c0\"><tr><td height=\"8\" width=\"8\">&nbsp;</td></tr></table></td>\n" \
-    "        </tr><tr>\n" \
-    "            <td width=\"8\" align=\"left\" valign=\"top\"><table width=\"8\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#c0c0c0\"><tr><td height=\"8\" width=\"8\">&nbsp;</td></tr></table></td>\n" \
-    "            <td><small>&nbsp;</small></td>\n" \
-    "        </tr>\n" \
-    "    </table>\n" \
-    "    </center>\n" \
-    "    <br>\n"
+#define PRT_HTMLFM_PAGEFOOTER "\n" \
+		    "</td></tr>\n" \
+		"</table>\n" \
+		"</td><td valign=\"top\" align=\"left\" width=\"8\"><table width=\"8\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#c0c0c0\"><tr><td height=\"8\" width=\"8\">&nbsp;</td></tr></table></td>\n" \
+	    "</tr><tr>\n" \
+		"<td width=\"8\" align=\"left\" valign=\"top\"><table width=\"8\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" bgcolor=\"#c0c0c0\"><tr><td height=\"8\" width=\"8\">&nbsp;</td></tr></table></td>\n" \
+		"<td><small>&nbsp;</small></td>\n" \
+	    "</tr>\n" \
+	"</table>\n" \
+	"</center>\n" \
+	"<br>\n"
 
 
 /*** this puts the min size at 9 (1), max size at 26 (7), and standard size at 12 (3) ***/
