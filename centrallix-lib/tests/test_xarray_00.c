2a0a390f86e597e45b17c4dfe6c76b7104a1c308
Lightning11wins <lightning11wins+github@gmail.com>
2026-09-08 16:11:22 -0600

    Fix memory leak.

--- centrallix-lib/tests/test_xarray_00.c
+++ centrallix-lib/tests/test_xarray_00.c
@@ -134,6 +134,7 @@ static bool do_tests(void)
 	/** Remove an item and ensure that it is gone. **/
 	success &= EXPECT_EQL(xaRemoveItem(xa, 2), 0, "%d"); /* Remove original index 2. */
 	success &= EXPECT_EQL(xaCount(xa), 5, "%d");
+	nmFree(v2, sizeof(int)); v2 = NULL; /* v2 is removed so it isn't freed with xaClear(). */
 
 	/** Count non-NULL items prior to clearing. **/
 	int count_before = xaCount(xa);
