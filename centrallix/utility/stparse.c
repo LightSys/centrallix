31a477f5b56a2cf8f9844a7cce298cb41be934cb
Lightning11wins <lightning11wins+github@gmail.com>
2026-09-18 17:20:47 -0600

    Update error handling in stparse.c.

--- centrallix/utility/stparse.c
+++ centrallix/utility/stparse.c
@@ -8,6 +8,7 @@
 #include "cxlib/exception.h"
 #include "stparse_ne.h"
 #include "stparse.h"
+#include "cxlib/check.h"
 #include "cxlib/mtsession.h"
 #include "cxlib/xstring.h"
 #include "cxlib/newmalloc.h"
@@ -590,15 +591,19 @@ stGetAttrValueOSML(pStructInf this, int type, pObjData pod, int nval, pObjSessio
     {
     pExpression find_exp;
     pParamObjects my_objlist = objlist;
+    int rval = -1;
 
-	/** Do some error-cascade checking. **/
-	if (!this) return -1;
-	
+	/** Handle edge cases. **/
+	if (this == NULL)
+	    {
+	    mssError(0, "ST", "stGetAttrValueOSML(NULL, ...) failed: Cannot get attribute value from null struct inf.");
+	    return -1; /* Skip end error handler because it uses this. */
+	    }
 	ASSERTMAGIC(this, MGK_STRUCTINF);
 
 	/** Get the correct expression **/
-	find_exp = stGetExpression(this, nval);
-	if (!find_exp) return -1;
+	find_exp = checkPtr(stGetExpression(this, nval));
+	if (find_exp == NULL) goto end;
 
 	/** expression code? **/
 	if (type == DATA_T_CODE && (find_exp->Flags & (EXPR_F_RUNCLIENT | EXPR_F_RUNSERVER)))
@@ -612,7 +617,7 @@ stGetAttrValueOSML(pStructInf this, int type, pObjData pod, int nval, pObjSessio
 	    {
 	    if (!objlist)
 		{
-		my_objlist = expCreateParamList();
+		my_objlist = checkPtr(expCreateParamList());
 		my_objlist->Session = sess;
 		}
 	    if (domain != 0)
@@ -624,9 +629,30 @@ stGetAttrValueOSML(pStructInf this, int type, pObjData pod, int nval, pObjSessio
 
 	/** Correct type requested? **/
 	if (find_exp->Flags & EXPR_F_NULL) return 1;
-	if (type != DATA_T_ANY && type != find_exp->DataType) return -1;
+	
+	/** Check for data type mismatch. **/
+	if (type != DATA_T_ANY && type != find_exp->DataType)
+	    {
+	    mssError(1, "ST",
+		"Expected ['%s' : %s], but got type %s.",
+		this->Name, objTypeToStr(type), objTypeToStr(find_exp->DataType)
+	    );
+	    goto end;
+	    }
 
-    return expExpressionToPod(find_exp, find_exp->DataType, pod);
+	/** Get the expression value. **/
+	rval = expExpressionToPod(find_exp, find_exp->DataType, pod);
+
+    end:
+        if (UNLIKELY(rval < 0))
+	    {
+	    mssError(1, "ST",
+		"Failed to parse attribute \"%s\" from group \"%s\"",
+		this->Name, this->Parent->Name
+	    );
+	    }
+
+	return rval;
     }
 
 
@@ -1787,5 +1813,3 @@ stPrint_ne(pStruct inf)
     {
     return stPrint_ne_r(inf, 0);
     }
-
-
