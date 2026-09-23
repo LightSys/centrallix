7a8008b113998fd7b27dcb20e439ae3c6a716733
Lightning11wins <lightning11wins+github@gmail.com>
2026-09-23 10:15:55 -0600

    Add expect.h markers to mssPrintError().

--- centrallix-lib/src/mtsession.c
+++ centrallix-lib/src/mtsession.c
@@ -621,12 +621,12 @@ mssPrintError(pFile fd)
     XString str; str.String = NULL;
     int rval = -1, tmp;
 
-	if (fd == NULL) goto end;
+	if (UNLIKELY(fd == NULL)) goto end;
 	if (warnFail(xsInit(&str))) goto end;
 
 	/** Format the stack once, so both error printers agree on the layout. **/
 	tmp = warnFail(mssStringError(&str));
-	if (tmp != 0)
+	if (UNLIKELY(tmp != 0))
 	    {
 	    rval = tmp;
 	    goto end;
@@ -637,7 +637,7 @@ mssPrintError(pFile fd)
 	rval = 0;
 
     end:
-	if (rval != 0) /* Make sure we print something if a failure happenned. */
+	if (UNLIKELY(rval != 0)) /* Make sure we print something if a failure happenned. */
 	    fprintf(stderr, "Warning: Failed to print session errors.\n");
 
 	/** Clean up. **/
