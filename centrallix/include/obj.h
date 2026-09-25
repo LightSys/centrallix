269efb5146775f1728e4ae7a80e3f9f6cb7908ef
Lightning11wins <lightning11wins+github@gmail.com>
2026-09-25 16:02:59 -0600

    Fix objFileName() breaking when called on root.

--- centrallix/include/obj.h
+++ centrallix/include/obj.h
@@ -632,11 +632,11 @@ typedef struct
 #define OBJ_MQ_F_NOUPDATE	(1<<1)		/* disallow any updates in this query */
 #define	OBJ_MQ_F_ONEROW		(1<<2)		/* only need first row from results */
 
-/** @returns The file name for the provided object. **/
+/** @returns The file name for the provided object, or "/" for the root. **/
 #define objFileName(obj) \
     ({ \
 	__typeof__ (obj) _obj = (obj); \
-	obj_internal_PathPart(_obj->Pathname, _obj->SubPtr - 1, 1); \
+	(_obj->SubPtr > 0) ? obj_internal_PathPart(_obj->Pathname, _obj->SubPtr - 1, 1) : "/"; \
     })
 /** @returns The file path to the provided object. **/
 #define objFilePath(obj) \
